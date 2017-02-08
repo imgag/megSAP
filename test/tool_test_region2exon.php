cabd8538e0a81f16ab9ecad312401396f152eae7
marc-sturm <marc.sturm@med.uni-tuebingen.de>
2017-02-08 07:38:20 +0100

    region2exon: refactoring

--- test/tool_test_region2exon.php
+++ test/tool_test_region2exon.php
@@ -5,12 +5,25 @@ require_once("framework.php");
 $name = "region2exon";
 start_test($name);
 
-//test
-$in1 = data_folder().$name."_in.txt";
-$out1 = output_folder().$name."_out.txt";
-check_exec("php ".src_folder()."/Primer/".$name.".php -in $in1 -out $out1");
-print "php ".src_folder()."/Primer/".$name.".php -in $in1 -out $out1";
-check_file($out1, data_folder().$name."_out.txt");
+//test 1
+$out1 = output_folder().$name."_out1.txt";
+check_exec("php ".src_folder()."/Primer/".$name.".php -reg chr1:151137550-151138268 -out $out1");
+check_file($out1, data_folder().$name."_out1.txt");
+
+//test 2
+$out2 = output_folder().$name."_out2.txt";
+check_exec("php ".src_folder()."/Primer/".$name.".php -reg chr17:41244361-41244377 -out $out2");
+check_file($out2, data_folder().$name."_out2.txt");
+
+//test 3
+$out3 = output_folder().$name."_out3.txt";
+check_exec("php ".src_folder()."/Primer/".$name.".php -reg chr1:151139409-151139498 -out $out3");
+check_file($out3, data_folder().$name."_out3.txt");
+
+//test 4
+$out4 = output_folder().$name."_out4.txt";
+check_exec("php ".src_folder()."/Primer/".$name.".php -reg chr17:40044361-40044377 -out $out4");
+check_file($out4, data_folder().$name."_out4.txt");
 
 end_test();
 
