31b0c34e0584fd953d77bad5095cee2689cffff6
Jakob Matthes <jakob.matthes@gmail.com>
2017-03-02 15:14:59 +0100

    tool_test_normalize_read_counts.php: adapated test to new parameters and file format

--- test/tool_test_normalize_read_counts.php
+++ test/tool_test_normalize_read_counts.php
@@ -12,13 +12,13 @@ $prefix = output_folder().$name;
 //test 1: rpkm normalization
 $out_file1 = output_folder().$name."_out1.tsv";
 $log_file1 = output_folder().$name."_out1.log";
-check_exec("php ".src_folder()."/NGS/{$name}.php -in '$in_file' -out '$out_file1' -method rpkm -header --log $log_file1");
+check_exec("php ".src_folder()."/NGS/{$name}.php -in '$in_file' -out '$out_file1' -method rpkm --log $log_file1");
 check_file($out_file1, data_folder().$name."_out1.tsv");
 
 //test 2: cpm normalization
 $out_file2 = output_folder().$name."_out2.tsv";
 $log_file2 = output_folder().$name."_out2.log";
-check_exec("php ".src_folder()."/NGS/{$name}.php -in '$in_file' -out '$out_file2' -method cpm -header --log $log_file2");
+check_exec("php ".src_folder()."/NGS/{$name}.php -in '$in_file' -out '$out_file2' -method cpm --log $log_file2");
 check_file($out_file2, data_folder().$name."_out2.tsv");
 
 end_test();
