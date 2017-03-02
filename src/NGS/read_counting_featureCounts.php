be0f2c4417f5cc17eef4f695543fee2043e3e0ed
Jakob Matthes <jakob.matthes@gmail.com>
2017-03-02 14:50:01 +0100

    read_counting_featureCounts.php: use temporary directory and fixed wrong option

--- src/NGS/read_counting_featureCounts.php
+++ src/NGS/read_counting_featureCounts.php
@@ -20,7 +20,7 @@ $parser->addString("gtfAttribute", "GTF attribute used as feature ID.", true, "g
 $parser->addFlag("stranded", "Specify whether a stranded protocol was used during library preparation. Default is non-stranded.");
 $parser->addInt("minAQual", "Minimal alignment quality. Skip all reads with alignment quality lower than the given value. For paired-end reads at least one end should satisfy this criteria.", true, 3);
 $parser->addFlag("paired", "The data is paired-end. Only properly paired reads are used.");
-$parser->addFlag("includeMultiOverlap", "Count also reads that overlap more than one feature.");
+$parser->addFlag("includeMultiOverlap", "Count reads multiple times if they overlap more than one feature.");
 $parser->addInt("threads", "Number of threads used for read counting", true, "4");
 extract($parser->parse($argv));
 
@@ -33,11 +33,16 @@ $args[] = "-Q $minAQual";
 $args[] = "-T $threads";
 $args[] = "-s ".($stranded ? "1" : "0");
 if($paired) $args[] = "-p -B";
-if($includeMultiOverlap) $args[] = "-M";
+if($includeMultiOverlap) $args[] = "-O";
+$tmp_dir = $parser->tempFolder();
+$tmp_out = $parser->tempFile();
+$args[] = "--tmpDir $tmp_dir";
+$args[] = "-o $tmp_out";
+$args[] = "$in";
 
 //execute command
-$parser->exec(get_path("feature_counts"), "$in -o $out ".implode(" ", $args), true);
+$parser->exec(get_path("feature_counts"), implode(" ", $args), true);
 
-// cleanup
-$parser->exec("rm ", "${out}.summary", false);
+// copy output
+$parser->exec("cp", "$tmp_out $out", true);
 ?>
