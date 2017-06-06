eb2c09dcb373b6effb128d641f118610fd65b2f5
Jakob Matthes <jakob.matthes@gmail.com>
2017-06-06 13:59:29 +0200

    updated read counting script tests

---
+++ test/tool_test_rc_featurecounts.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once("framework.php");
+require_once(dirname($_SERVER['SCRIPT_FILENAME'])."/../src/Common/genomics.php");
+
+$name = "rc_featurecounts";
+start_test($name);
+
+
+$out = output_folder().$name."_out1.tsv";
+check_exec("php ".src_folder()."/NGS/{$name}.php -in ".data_folder().$name."_in.bam -out $out -paired --log ".output_folder().$name."_out1.log");
+remove_lines_containing($out, array("Program:featureCounts", "rc_featurecounts_in.bam"));
+check_file($out, data_folder().$name."_out1.tsv", false);
+
+end_test();
+
+?>
