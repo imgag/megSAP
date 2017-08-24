8b21f414532841c8f0aa944facacd74ca2b97c84
Jakob Matthes <jakob.matthes@gmail.com>
2017-08-24 15:37:43 +0200

    rc_featurecounts.php: implement correct library type argument

--- src/NGS/rc_featurecounts.php
+++ src/NGS/rc_featurecounts.php
@@ -14,32 +14,46 @@ $parser->addInfile("in",  "BAM input file.", false, true);
 $parser->addOutfile("out", "Raw count output TSV file.", false);
 
 //optional parameters
-$parser->addString("gtfFile", "GTF File containing feature annotations used for read counting.", true, get_path("data_folder")."/dbs/gene_annotations/GRCh37.gtf");
-$parser->addString("featureType", "Feature type used for mapping reads to features.", true, "exon");
-$parser->addString("gtfAttribute", "GTF attribute used as feature ID.", true, "gene_id");
-$parser->addFlag("stranded", "Specify whether a stranded protocol was used during library preparation. Default is non-stranded.");
-$parser->addInt("minAQual", "Minimal alignment quality. Skip all reads with alignment quality lower than the given value. For paired-end reads at least one end should satisfy this criteria.", true, 3);
-$parser->addFlag("paired", "The data is paired-end. Only properly paired reads are used.");
-$parser->addFlag("includeMultiOverlap", "Count reads multiple times if they overlap more than one feature.");
-$parser->addFlag("keepSummary", "Keep summary file.");
+$parser->addString("gtf_file", "GTF file containing feature annotations used for read counting.", true, get_path("data_folder")."/dbs/gene_annotations/GRCh37.gtf");
+$parser->addString("feature_type", "Feature type used for mapping reads to features.", true, "exon");
+$parser->addString("gtf_attribute", "Attribute used as feature ID.", true, "gene_id");
+
+$parser->addEnum("library_type", "Specify the library type, i.e. the strand R1 originates from (dUTP libraries correspond to reverse).", true, array("unstranded", "reverse", "forward"), "reverse");
+$parser->addFlag("single_end", "Single-end data.");
+
+$parser->addInt("min_mapq", "Minimal mapping quality.", true, 3);
+
+$parser->addFlag("overlap", "Count reads multiple times if they overlap more than one feature.");
+
+$parser->addFlag("keep_summary", "Keep summary file.");
+
 $parser->addInt("threads", "Number of threads used for read counting", true, 4);
 extract($parser->parse($argv));
 
-//build arguments array
-$args = array();
-$args[] = "-a $gtfFile";
-$args[] = "-t $featureType";
-$args[] = "-g $gtfAttribute";
-$args[] = "-Q $minAQual";
-$args[] = "-T $threads";
-$args[] = "-s ".($stranded ? "2" : "0");
-if($paired) $args[] = "-p -B";
-if($includeMultiOverlap) $args[] = "-O";
+$strandedness = array(
+	"unstranded" => 0,
+	"reverse" => 2,
+	"forward" => 1
+);
+
 $tmp_dir = $parser->tempFolder();
 $tmp_out = $parser->tempFile();
-$args[] = "--tmpDir $tmp_dir";
-$args[] = "-o $tmp_out";
-$args[] = "$in";
+
+//build arguments array
+$args = array(
+	"-a", $gtf_file,
+	"-t", $feature_type,
+	"-g", $gtf_attribute,
+	"-Q", $min_mapq,
+	"-T", $threads,
+	"-s", $strandedness[$library_type],
+	"--tmpDir", $tmp_dir,
+	"-o", $tmp_out
+);
+
+if(!$single_end) $args[] = "-p -B";
+if($overlap) $args[] = "-O";
+$args[] = $in;
 
 //execute command
 $parser->exec(get_path("feature_counts"), implode(" ", $args), true);
@@ -47,7 +61,7 @@ $parser->exec(get_path("feature_counts"), implode(" ", $args), true);
 // copy output
 $parser->exec("cp", "$tmp_out $out", true);
 
-if ($keepSummary)
+if ($keep_summary)
 {
 	$out_summary = dirname($out)."/".basename($out, ".tsv")."_summary.tsv";
 	$parser->exec("cp", "{$tmp_out}.summary {$out_summary}", true);
