768aeaf508f611d9c0fed9c6e2dd3296dad32a1f
Jakob Matthes <jakob.matthes@gmail.com>
2017-08-29 15:22:03 +0200

    mapping.php: keep output from FastqExtractBarcode

--- src/Pipelines/mapping.php
+++ src/Pipelines/mapping.php
@@ -67,9 +67,9 @@ else
 
 // MIPs: move molecular barcode to separate file
 $index_file = $basename."_index.fastq.gz";
+$trimmed_mips2 = $basename."_MB_001.fastq.gz";
 if($sys['type']=="Panel MIPs")
 {
-	$trimmed_mips2 = $parser->tempFile("_mips_indices.fastq.gz"); ///@todo rename to [sample]_MB_001.fastq.gz and store to sample folder
 	$parser->exec(get_path("ngs-bits")."FastqExtractBarcode", "-in $trimmed2 -out_main $trimmed_mips2 -cut 8 -out_index $index_file",true);
 	$trimmed2 = $trimmed_mips2;
 }
@@ -171,7 +171,7 @@ if($sys['type']=="Panel MIPs")
 	$mip_file = isset($sys['mip_file']) ? $sys['mip_file'] : "/mnt/share/data/mipfiles/".$sys["name_short"].".txt";
 	$bam_dedup1 = $parser->tempFile("_dedup1.bam");
 	// removed: 
-	copy2($out, $out."_before_dedup.bam");
+	copy2($out, basename($out, ".bam")."_before_dedup.bam");
 	$parser->exec(get_path("samtools"), "index ".$out."_before_dedup.bam", true);
 
 	$parser->exec(get_path("ngs-bits")."BamDeduplicateByBarcode", " -bam $out -index $index_file -mip_file $mip_file -out $bam_dedup1 -stats ".$basename."_bar_stats.tsv -del_amb  -dist 1", true);
