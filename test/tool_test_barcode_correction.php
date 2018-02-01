4ab79d87e494ba0a857e5ec5ac6636a6bf8192fc
Jakob Matthes <jakob.matthes@gmail.com>
2018-02-01 15:12:58 +0100

    removed python interpreter from settings, updated requirements

--- test/tool_test_barcode_correction.php
+++ test/tool_test_barcode_correction.php
@@ -6,7 +6,7 @@ $name = "barcode_correction";
 start_test($name);
 
 $out_file1 = output_folder().$name."_out1.bam";
-check_exec(get_path("python") . " ".src_folder()."/NGS/barcode_correction.py --infile ".data_folder().$name."_in1.bam --outfile $out_file1 --BCerror 1 --minBQ 30");
+check_exec("python ".src_folder()."/NGS/barcode_correction.py --infile ".data_folder().$name."_in1.bam --outfile $out_file1 --BCerror 1 --minBQ 30");
 check_file($out_file1, data_folder().$name."_out1.bam");
 
 end_test();
