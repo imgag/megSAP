1fcfd678c59ae97600bba762fff09fcc1b334531
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2018-02-14 15:23:56 +0100

    converter_Hgvs2Genomic: now works

--- src/Tools/converter_Hgvs2Genomic.php
+++ src/Tools/converter_Hgvs2Genomic.php
@@ -5,9 +5,9 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 require_once(dirname($_SERVER['SCRIPT_FILENAME'])."/../Common/all.php");
 
 // parse command line arguments
-$parser = new ToolBase("converter_Hgsv2Genomic", "Convert HGSV coordinates to genomic positions. Prints convertes position to stdout.");
+$parser = new ToolBase("converter_Hgvs2Genomic", "Convert HGSV coordinates to genomic positions. Prints convertes position to stdout.");
 $parser->addString("refseq",  "Refseq transcript ID.", false);
-$parser->addOutfile("hgvs",  "cDNA position in HGVS format.", false);
+$parser->addString("hgvs",  "cDNA position in HGVS format.", false);
 extract($parser->parse($argv));
 
 list($chr,$start,$end,$ref,$obs) = convert_hgvs2genomic($refseq, $hgvs);
