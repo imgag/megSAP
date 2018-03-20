483dcf1a2ea1dd3a04f9c3a590d83475998e3937
Jakob Matthes <jakob.matthes@gmail.com>
2018-03-20 08:33:07 +0100

    updated manta to 1.3.2

--- data/download_tools_somatic.sh
+++ data/download_tools_somatic.sh
@@ -14,8 +14,8 @@ cd strelka-2.8.4.centos6_x86_64
 
 #download manta
 cd $folder
-wget https://github.com/Illumina/manta/releases/download/v1.2.2/manta-1.2.2.centos6_x86_64.tar.bz2
-tar xjf manta-1.2.2.centos6_x86_64.tar.bz2
-rm manta-1.2.2.centos6_x86_64.tar.bz2
-cd manta-1.2.2.centos6_x86_64
+wget https://github.com/Illumina/manta/releases/download/v1.3.2/manta-1.3.2.centos6_x86_64.tar.bz2
+tar xjf manta-1.3.2.centos6_x86_64.tar.bz2
+rm manta-1.3.2.centos6_x86_64.tar.bz2
+cd manta-1.3.2.centos6_x86_64
 sed -i 's#referenceFasta = /illumina/development/Isis/Genomes/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa##g' bin/configManta.py.ini
