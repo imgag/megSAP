71e6e62d335fe28fd8053a4aad878231d2f52dc9
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2018-07-17 08:56:19 +0200

    db_check_gender: improved output

--- src/NGS/db_check_gender.php
+++ src/NGS/db_check_gender.php
@@ -1,17 +1,15 @@
 <?php
 /** 
 	@page db_check_gender
-	
-	@todo Also use SRY-based method for panels that contain the SRY gene
 */
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 require_once(dirname($_SERVER['SCRIPT_FILENAME'])."/../Common/all.php");
 
-$parser = new ToolBase("db_check_gender", "Checks that the gender of a sample matches the DB meta information.");
+$parser = new ToolBase("db_check_gender", "Checks the gender of a sample.");
 $parser->addInfile("in",  "Input file in BAM format.", false);
-$parser->addString("pid",  "Processed sample ID, e.g. GS120001_01, used to determine the sample gender from the DB.", false);
-$parser->addString("gender", "Gender of the input. If unset, gender is looked up in NGSD.",true);
+$parser->addString("pid",  "Processed sample ID, e.g. GS120001_01, used to determine the sample gender from the NGSD.", false);
+$parser->addString("gender", "Gender of the input. If unset, gender is looked up in NGSD.", true);
 //optional
 $parser->addEnum("db",  "Database to connect to.", true, db_names(), "NGSD");
 extract($parser->parse($argv));
@@ -45,22 +43,19 @@ else
 	}
 }
 
-//execute SampleGender
-if ($sry_in_roi) 
+//determine gender from BAM
+$extra = "";
+if ($sry_in_roi)
 {
-	list($stdout, $stderr) = $parser->exec(get_path("ngs-bits")."SampleGender", "-method sry -sry_cov 10 -in $in", true);
+	$method = "sry";
+	$extra = "-sry_cov 10";
 }
 else
 {
-	list($stdout, $stderr) = $parser->exec(get_path("ngs-bits")."SampleGender", "-method hetx -in $in", true);
+	$method = "hetx";
 }
-
-//determine gender
+list($stdout, $stderr) = $parser->exec(get_path("ngs-bits")."SampleGender", "-in {$in} -method {$method} {$extra}", true);
 $gender2 = explode("\t", $stdout[1])[1];
-if ($gender2=="")
-{
-	trigger_error("Could not determine gender from SampleGender output!", E_USER_ERROR);
-}
 if (starts_with($gender2, "unknown"))
 {
 	$parser->log("Could not check gender, because we could not determine it from BAM file.");
@@ -70,7 +65,7 @@ if (starts_with($gender2, "unknown"))
 //make sure they match
 if ($gender!=$gender2)
 {
-	trigger_error("Gender from DB '$gender' does not match gender from BAM file '$gender2'!", E_USER_ERROR);
+	trigger_error("Expected gender '{$gender}', but determined gender '{$gender2}' using method {$method} on BAM file {$in}", E_USER_ERROR);
 }
 
 ?>
