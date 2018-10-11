761d4e7f7962daec0be55167738c330be2a09e19
Axel Gschwind <axel.gschwind@t-online.de>
2018-10-11 12:41:03 +0200

    somatic: Tested new version of MANTA 1.4.0 and STRELKA 2.9.9, adapted test cases and updated download_tool_somatic.sh

--- data/download_tools_somatic.sh
+++ data/download_tools_somatic.sh
@@ -6,17 +6,17 @@ folder=`pwd`/tools/
 cd $folder
 
 #downloada strelka2
-wget https://github.com/Illumina/strelka/releases/download/v2.8.4/strelka-2.8.4.centos6_x86_64.tar.bz2
-tar xjf strelka-2.8.4.centos6_x86_64.tar.bz2
-rm strelka-2.8.4.centos6_x86_64.tar.bz2
+wget https://github.com/Illumina/strelka/releases/download/v2.9.9/strelka-2.9.9.centos6_x86_64.tar.bz2
+tar xjf strelka-2.9.9.centos6_x86_64.tar.bz2
+rm strelka-2.9.9.centos6_x86_64.tar.bz2
 
 
 #download manta
 cd $folder
-wget https://github.com/Illumina/manta/releases/download/v1.3.2/manta-1.3.2.centos6_x86_64.tar.bz2
-tar xjf manta-1.3.2.centos6_x86_64.tar.bz2
-rm manta-1.3.2.centos6_x86_64.tar.bz2
-cd manta-1.3.2.centos6_x86_64
+wget https://github.com/Illumina/manta/releases/download/v1.4.0/manta-1.4.0.centos6_x86_64.tar.bz2
+tar xjf manta-1.4.0.centos6_x86_64.tar.bz2
+rm manta-1.4.0.centos6_x86_64.tar.bz2
+cd manta-1.4.0.centos6_x86_64.tar.bz2
 sed -i 's#referenceFasta = /illumina/development/Isis/Genomes/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa##g' bin/configManta.py.ini
 
 
