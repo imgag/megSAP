b268f4cfc2b08876e867e9f748c18810216a0e0e
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2018-12-05 15:25:23 +0100

    Updated GenLab access (new views)

--- src/Tools/genlab_gender.php
+++ src/Tools/genlab_gender.php
@@ -3,45 +3,42 @@
 require_once(dirname($_SERVER['SCRIPT_FILENAME'])."/../Common/all.php");
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
+function gender2human($gender_code)
+{	
+	$gender_code = trim($gender_code);
+	if($gender_code=="1")
+	{
+		return "female";
+	}
+	if($gender_code=="2")
+	{
+		return "male";
+	}
+	
+	return "n/a ($gender_code)";
+}
+
 //parse command line arguments
 $parser = new ToolBase("genlab_gender", "Returns gender of sample ID from Genlab.");
-$parser->addOutFile("id","Sample ID (e.g DX152541_01).",false);
+$parser->addOutFile("id","Sample ID (e.g DX152541/DX152541_01).",false);
 //optional
 extract($parser->parse($argv));
+
+//check DB is enabled
 if(!db_is_enabled("GL8"))
 {
 	trigger_error("Could not connect to GenLab database.", E_USER_ERROR);
 }
 
+//get gender
 $db = DB::getInstance("GL8");
-
-//Resolve patient ID from GenLab
-$patient_ids = $db->getValues("SELECT PATIENT_ID FROM t_untersuchung WHERE labornummer = '{$id}'");
-$patient_ids = array_unique($patient_ids);
-if(count($patient_ids)!=1)
+$res = $db->executeQuery("SELECT geschlecht,identnr FROM v_ngs_geschlecht WHERE labornummer = '{$id}'");
+if(empty($res))
 {
-	trigger_error("Found no/multiple patient IDs for sample ID {$id}: ".implode(", ", $patient_ids), E_USER_ERROR);
+	print "No gender in GenLab for '$id'.\n";
 }
-$patient_id = $patient_ids[0];
-
-//Resolve Gender
-$res2 = $db->executeQuery("SELECT GESCHLECHT FROM t_patient WHERE id = '{$patient_id}'");
-if(empty($res2))
+foreach($res as $row)
 {
-	trigger_error("No gender in GenLab for patient ID {$patient_id}.", E_USER_ERROR);
-}
-
-$gender_code = $res2[0]["GESCHLECHT"];
-if($gender_code == 1) //1:female
-{
-	print("female\n");
-}
-elseif($gender_code == 2) //2:male
-{
-	print("male\n");
-}
-else
-{
-	print("n/a\n");
+	print "gender: ".gender2human($row["geschlecht"])." (identnr: ".$row["identnr"].")\n";
 }
 ?>
