4046d540fb57490529975b053bd79155e3eadee6
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2019-04-02 12:56:14 +0200

    low_cov_regions: no longer needs roi parameter + bug fixes

--- src/NGS/low_cov_regions.php
+++ src/NGS/low_cov_regions.php
@@ -5,50 +5,57 @@ require_once(dirname($_SERVER['SCRIPT_FILENAME'])."/../Common/all.php");
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 $parser = new ToolBase("low_cov_regions", "Determines regions that have low coverage in a large part of samples.");
-$parser->addInfileArray("in", "Low-coverage BED files for several samples.", false);
-$parser->addInfile("roi", "Target region BED file.", false);
+$parser->addInfileArray("in", "Low-coverage BED files for several samples OR one file containing file names.", false);
 $parser->addOutfile("out", "Output BED file with low-coverage regions.", false);
 //optional
 $parser->addInt("percentile", "Percentile of samples with low coverage needed for output.", true, 40);
 $parser->addFlag("tumor", "Only process tumor samples, otherwise only non-tumor samples are process).", true, 40);
 extract($parser->parse($argv));
-	
 
-// init overall coverage statistics
-$low = array();
-$regs = load_tsv($roi);
-foreach($regs as $reg)
+//load input file names
+if (count($in)==1)
 {
-	list($c, $s, $e) = $reg;
-	for($p=$s; $p<$e; ++$p)
-	{
-		$low[$c."_".$p] = 0;
-	}
+	$in = file($in[0]);
 }
 
-// process input files
+//process input files
+$samples_processed = 0;
 $db = DB::getInstance("NGSD");
 foreach($in as $bed)
 {
-	//filter out or select tumor samples
-	list($sample) = explode("_", basename($bed));
-	$sample_tumor = $db->getValue("SELECT tumor FROM sample WHERE name='$sample'", "n/a");
-	if ($sample_tumor=="n/a")
+	$bed = trim($bed);
+	if ($bed=="") continue;
+	
+	//check quality
+	$base = basename($bed, "_lowcov.bed");
+	$info = get_processed_sample_info($db, $base, false);
+	if(is_null($info))
 	{
-		trigger_error("Could not determine if sample '$sample' is a tumor sample. Skipping it!", E_USER_WARNING);
+		print "skipping: $base (not found in NGDS)\n";
 		continue;
 	}
-	if ($tumor && $sample_tumor=="0")
+	if ($info['ps_quality']!="good")
 	{
-		continue;
-	}
-	else if (!$tumor && $sample_tumor=="1")
-	{
+		print "skipping: $base (not 'good' quality)\n";
 		continue;
 	}
 	
+	//filter out or select tumor samples
+	if ($tumor && !$info['is_tumor'])
+	{
+		print "skipping: $base (is non-tumor sample)\n";
+		continue;
+	}
+	else if (!$tumor && $info['is_tumor'])
+	{
+		print "skipping: $base (is tumor sample)\n";
+		continue;
+	}
+	
+	++$samples_processed;
+	
 	//process low-coverage file
-	print "processing: $bed\n";
+	print "processing: $base\n";
 	$file = file($bed);
 	foreach($file as $line)
 	{
@@ -59,8 +66,7 @@ foreach($in as $bed)
 		{
 			if (!isset($low[$chr."_".$p]))
 			{
-				continue;
-				trigger_error("Found position $chr:$p in input BED file '$bed', that is not part of the ROI '$roi'!", E_USER_ERROR);
+				$low[$chr."_".$p] = 0;
 			}
 			$low[$chr."_".$p] += 1;
 		}
@@ -68,8 +74,7 @@ foreach($in as $bed)
 }
 
 //calculate threshold
-$thres = count($in) * $percentile / 100.0;
-print "threshold: $thres\n";
+$thres = number_format($samples_processed * $percentile / 100.0, 2);
 
 //write output BED file
 $output = array();
@@ -88,5 +93,13 @@ $parser->exec(get_path("ngs-bits")."BedMerge", "-in $out -out $out", false);
 
 //annotate with gene names
 $parser->exec(get_path("ngs-bits")."BedAnnotateGenes", "-in $out -clear -out $out", false);
+
+//output
+print "input files: ".count($in)."\n";
+print "threshold: {$thres} of {$samples_processed} processed samples\n";
+list($stdout, $stderr) = exec2(get_path("ngs-bits")."BedInfo -in $out | grep Bases");
+list(,$bases) = explode(":", $stdout[0]);
+print "low-coverage bases: ".number_format($bases, 0)."\n";
+ 
 
 ?>
