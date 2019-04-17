a786027fe4ee6e59d106fb3f3ccedff56088f0ef
Jakob Matthes <jakob.matthes@gmail.com>
2019-04-17 09:30:29 +0200

    updated RNA tools and databases

--- data/download_tools_rna.sh
+++ data/download_tools_rna.sh
@@ -8,29 +8,28 @@ folder=`pwd`/tools/
 cd $folder
 
 #download STAR
-git clone --depth 1 --branch 2.6.1a https://github.com/alexdobin/STAR.git STAR-2.6.1a
-cd STAR-2.6.1a
-git checkout 2.6.1a
+wget https://github.com/alexdobin/STAR/archive/2.7.0f.tar.gz
+tar xzf 2.7.0f.tar.gz
+rm 2.7.0f.tar.gz
 
 #download STAR-Fusion
-wget https://github.com/STAR-Fusion/STAR-Fusion/releases/download/STAR-Fusion-v1.4.0/STAR-Fusion-v1.4.0.FULL.tar.gz
-tar xzf STAR-Fusion-v1.4.0.FULL.tar.gz
-rm STAR-Fusion-v1.4.0.FULL.tar.gz
+wget https://github.com/STAR-Fusion/STAR-Fusion/releases/download/v1.6.0/STAR-Fusion-v1.6.0.FULL.tar.gz
+tar xzf STAR-Fusion-v1.6.0.FULL.tar.gz
+rm STAR-Fusion-v1.6.0.FULL.tar.gz
 
-#download subread (featureCounts, subjunc)
-wget http://downloads.sourceforge.net/project/subread/subread-1.6.2/subread-1.6.2-Linux-x86_64.tar.gz
-tar xzf subread-1.6.2-Linux-x86_64.tar.gz
-rm subread-1.6.2-Linux-x86_64.tar.gz
-mv subread-1.6.2-Linux-x86_64 subread-1.6.2
+#download subread (featureCounts)
+wget http://downloads.sourceforge.net/project/subread/subread-1.6.4/subread-1.6.4-Linux-x86_64.tar.gz
+tar xzf subread-1.6.4-Linux-x86_64.tar.gz
+rm subread-1.6.4-Linux-x86_64.tar.gz
+mv subread-1.6.4-Linux-x86_64 subread-1.6.4
 
 #download skewer (single-end adapter trimming)
 git clone --depth 1 --branch 0.2.2 https://github.com/relipmoc/skewer.git skewer-0.2.2
 cd skewer-0.2.2
-git checkout 0.2.2
 make
 
 #download salmon
-wget https://github.com/COMBINE-lab/salmon/releases/download/v0.11.3/salmon-0.11.3-linux_x86_64.tar.gz
-tar xzf salmon-0.11.3-linux_x86_64.tar.gz
-rm salmon-0.11.3-linux_x86_64.tar.gz
-mv salmon-0.11.3-linux_x86_64 salmon-0.11.3
+wget https://github.com/COMBINE-lab/salmon/releases/download/v0.13.1/salmon-0.13.1_linux_x86_64.tar.gz
+tar xzf salmon-0.13.1_linux_x86_64.tar.gz
+rm salmon-0.13.1_linux_x86_64.tar.gz
+mv salmon-latest_linux_x86_64 salmon-0.13.1
