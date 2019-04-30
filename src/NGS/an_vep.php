b0e609c44e4a832bdd1575d6599036c0398bf40a
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2019-04-30 10:50:25 +0200

    an_vep/data_setup: VEP annotation data is now used from local tmp folder

--- src/NGS/an_vep.php
+++ src/NGS/an_vep.php
@@ -20,12 +20,37 @@ $parser->addFlag("all_transcripts", "Annotate all transcripts - if unset only GE
 $parser->addInt("threads", "The maximum number of threads used.", true, 1);
 extract($parser->parse($argv));
 
+//get local/global data file path - depending on what is available
+function annotation_file_path($rel_path)
+{
+	global $data_folder;
+	global $local_data;
+	
+	//check if original exists
+	$orig = $data_folder.$rel_path;
+	if (!file_exists($orig))
+	{
+		trigger_error("VEP annotation file '$orig' missing!", E_USER_ERROR);
+	}
+	
+	//check if copy exists
+	$copy = $local_data."/ensembl-vep-dbs/".basename($rel_path);
+	if (!file_exists($copy))
+	{
+		trigger_error("VEP annotation file '$rel_path' not found in local data copy. Using (possibly slow) remote file '$orig'!", E_USER_NOTICE);
+		return $orig;
+	}
+	
+	return $copy;
+}
+
 //annotate only fields we really need to prevent bloating the VCF file 
 $fields = array("Allele", "Consequence", "IMPACT", "SYMBOL", "HGNC_ID", "Feature", "Feature_type", "EXON", "INTRON", "HGVSc", "HGVSp", "DOMAINS", "SIFT", "PolyPhen", "Existing_variation", "AF", "gnomAD_AF", "gnomAD_AFR_AF", "gnomAD_AMR_AF", "gnomAD_EAS_AF", "gnomAD_NFE_AF", "gnomAD_SAS_AF", "EA_AF", "AA_AF"); 
 
 $vep_path = dirname(get_path("vep"));
 $local_data = get_path("local_data");
 $vep_data_path = "{$local_data}/".basename(get_path("vep_data"))."/"; //the data is copied to the local data folder by 'data_setup' to speed up annotations (and prevent hanging annotation jobs)
+$data_folder = get_path("data_folder");
 
 $args = array();
 $args[] = "-i $in --format vcf"; //input
@@ -38,11 +63,11 @@ $args[] = "--regulatory"; //regulatory features
 $fields[] = "BIOTYPE"; 
 $args[] = "--sift b --polyphen b"; //pathogenicity predictions
 $args[] = "--af --af_gnomad --af_esp --failed 1"; //population frequencies
-$args[] = "--plugin CADD,".get_path("data_folder")."/dbs/CADD/whole_genome_SNVs.tsv.gz,".get_path("data_folder")."/dbs/CADD/InDels.tsv.gz"; //CADD
+$args[] = "--plugin CADD,".annotation_file_path("/dbs/CADD/whole_genome_SNVs.tsv.gz").",".annotation_file_path("/dbs/CADD/InDels.tsv.gz"); //CADD
 $fields[] = "CADD_PHRED";
-$args[] = "--plugin REVEL,".get_path("data_folder")."/dbs/REVEL/revel_all_chromosomes.tsv.gz"; //REVEL
+$args[] = "--plugin REVEL,".annotation_file_path("/dbs/REVEL/revel_all_chromosomes.tsv.gz"); //REVEL
 $fields[] = "REVEL";
-$args[] = "--plugin FATHMM_MKL,".get_path("data_folder")."/dbs/fathmm-MKL/fathmm-MKL_Current.tab.gz"; //fathmm-MKL
+$args[] = "--plugin FATHMM_MKL,".annotation_file_path("/dbs/fathmm-MKL/fathmm-MKL_Current.tab.gz"); //fathmm-MKL
 $fields[] = "FATHMM_MKL_C";
 $fields[] = "FATHMM_MKL_NC";
 $args[] = "--plugin MaxEntScan,{$vep_path}/MaxEntScan/"; //MaxEntScan
@@ -50,27 +75,27 @@ $fields[] = "MaxEntScan_ref";
 $fields[] = "MaxEntScan_alt";
 $args[] = "--plugin GeneSplicer,{$vep_path}/GeneSplicer/sources/genesplicer,{$local_data}/GeneSplicer/,context=50"; //GeneSplicer
 $fields[] = "GeneSplicer";
-$args[] = "--plugin dbscSNV,".get_path("data_folder")."/dbs/dbscSNV/dbscSNV1.1_GRCh37.txt.gz"; //dbscSNV
+$args[] = "--plugin dbscSNV,".annotation_file_path("/dbs/dbscSNV/dbscSNV1.1_GRCh37.txt.gz"); //dbscSNV
 $fields[] = "ada_score";
 $fields[] = "rf_score";
-$args[] = "--custom ".get_path("data_folder")."/dbs/gnomAD/gnomAD_genome_r2.1.vcf.gz,gnomADg,vcf,exact,0,AF,Hom,Hemi"; //genomAD
+$args[] = "--custom ".annotation_file_path("/dbs/gnomAD/gnomAD_genome_r2.1.vcf.gz").",gnomADg,vcf,exact,0,AF,Hom,Hemi"; //genomAD
 $fields[] = "gnomADg_AF";
 $fields[] = "gnomADg_Hom";
 $fields[] = "gnomADg_Hemi";
-$args[] = "--custom ".get_path("data_folder")."/dbs/RepeatMasker/RepeatMasker.bed.gz,REPEATMASKER,bed,overlap,0"; //RepeatMasker
+$args[] = "--custom ".annotation_file_path("/dbs/RepeatMasker/RepeatMasker.bed.gz").",REPEATMASKER,bed,overlap,0"; //RepeatMasker
 $fields[] = "REPEATMASKER";
-$args[] = "--custom ".get_path("data_folder")."/dbs/ClinVar/clinvar_20190123_converted.vcf.gz,CLINVAR,vcf,exact,0,DETAILS"; //ClinVar
+$args[] = "--custom ".annotation_file_path("/dbs/ClinVar/clinvar_20190123_converted.vcf.gz").",CLINVAR,vcf,exact,0,DETAILS"; //ClinVar
 $fields[] = "CLINVAR";
 $fields[] = "CLINVAR_DETAILS";
-$args[] = "--custom ".get_path("data_folder")."/dbs/phyloP/hg19.100way.phyloP100way.bw,PHYLOP,bigwig"; //phyloP
+$args[] = "--custom ".annotation_file_path("/dbs/phyloP/hg19.100way.phyloP100way.bw").",PHYLOP,bigwig"; //phyloP
 $fields[] = "PHYLOP";
-$omim_file = get_path("data_folder")."/dbs/OMIM/omim.bed.gz"; //OMIM annotation (optional because of license)
+$omim_file = annotation_file_path("/dbs/OMIM/omim.bed.gz"); //OMIM annotation (optional because of license)
 if(file_exists($omim_file))
 {
 	$args[] = "--custom {$omim_file},OMIM,bed,overlap,0";
 	$fields[] = "OMIM";
 }
-$hgmd_file = get_path("data_folder")."/dbs/HGMD/HGMD_PRO_2018_4_fixed.vcf.gz"; //HGMD annotation (optional because of license)
+$hgmd_file = annotation_file_path("/dbs/HGMD/HGMD_PRO_2018_4_fixed.vcf.gz"); //HGMD annotation (optional because of license)
 if(file_exists($hgmd_file))
 {
 	$args[] = "--custom {$hgmd_file},HGMD,vcf,exact,0,CLASS,MUT,GENE,PHEN";
--- src/Tools/data_setup.php
+++ src/Tools/data_setup.php
@@ -14,40 +14,41 @@ extract($parser->parse($argv));
 
 
 //init
-$local_folder = get_path("local_data");
-$genome_folder = get_path("data_folder")."/genomes/";
+$data_folder = get_path("data_folder");
+$local_data = get_path("local_data");
 $rsync = "rsync --archive --omit-dir-times --acls --no-perms --no-group --no-owner --chmod=ugo=rwX";
 
 ######################### reference genome #########################
+$genome_folder = "{$data_folder}/genomes/";
 print "### Reference genome ###\n";
 print "from: {$genome_folder}\n";
-print "to  : {$local_folder}\n";
+print "to  : {$local_data}\n";
 print "\n";
 
 //skip if they are the same
-if (realpath($local_folder)==realpath($genome_folder))
+if (realpath($local_data)==realpath($genome_folder))
 {
 	print "Skipping, because local and remote genome folders are the same!\n";
 	return;
 }
 
 //make local folder
-if (!file_exists($local_folder))
+if (!file_exists($local_data))
 {
-	if (!mkdir($local_folder))
+	if (!mkdir($local_data))
 	{
-		trigger_error("Could not create local data folder '{$local_folder}'!", E_USER_ERROR);
+		trigger_error("Could not create local data folder '{$local_data}'!", E_USER_ERROR);
 	}
-	if (!chmod($local_folder, 0777))
+	if (!chmod($local_data, 0777))
 	{
-		trigger_error("Could not change privileges of local data folder '{$local_folder}'!", E_USER_ERROR);
+		trigger_error("Could not change privileges of local data folder '{$local_data}'!", E_USER_ERROR);
 	}
 }
 
 //remove outdated genome FASTA files
-if (file_exists("{$local_folder}/{$build}.fa.md5"))
+if (file_exists("{$local_data}/{$build}.fa.md5"))
 {
-	exec("diff {$local_folder}/{$build}.fa.md5 {$genome_folder}/{$build}.fa.md5", $output, $code);
+	exec("diff {$local_data}/{$build}.fa.md5 {$genome_folder}/{$build}.fa.md5", $output, $code);
 	if ($code==0)
 	{
 		print "MD5 checksums of genome '{$build}.fa.md5' match.\n";
@@ -55,12 +56,12 @@ if (file_exists("{$local_folder}/{$build}.fa.md5"))
 	else
 	{
 		print "MD5 checksums of genome '{$build}.fa.md5' differ. Deleting old data!\n";
-		exec2("rm -rf {$local_folder}/{$build}.*");
+		exec2("rm -rf {$local_data}/{$build}.*");
 	}
 }
 
 //wait sync is done by another processes
-$pid_file = "{$local_folder}/megSAP_data_setup_{$build}.txt";
+$pid_file = "{$local_data}/megSAP_data_setup_{$build}.txt";
 if (file_exists($pid_file))
 {
 	$pid_old = trim(file_get_contents($pid_file));
@@ -68,7 +69,7 @@ if (file_exists($pid_file))
 	while (posix_getpgid($pid_old)!==FALSE)
 	{
 		++$iter;
-		if ($iter>30) break; //wait for 30 min max
+		if ($iter>60) break; //wait for 60 min max
 		print "Process with PID {$pid_old} is already syncing. Waiting one minute...\n";
 		sleep(60);
 		
@@ -93,8 +94,8 @@ foreach($files as $file)
 {
 	$base = basename($file);
 	print "  rsync-ing genome file '$base'.\n";
-	$existed_before = file_exists($local_folder.$base);
-	list($stdout, $stderr) = exec2("{$rsync} {$genome_folder}{$base} {$local_folder}{$base}");
+	$existed_before = file_exists($local_data.$base);
+	list($stdout, $stderr) = exec2("{$rsync} {$genome_folder}{$base} {$local_data}{$base}");
 	foreach(array_merge($stdout, $stderr) as $line)
 	{
 		$line = trim($line);
@@ -103,21 +104,21 @@ foreach($files as $file)
 	}
 	if (!$existed_before)
 	{
-		if (!chmod($local_folder.$base, 0777))
+		if (!chmod($local_data.$base, 0777))
 		{
-			trigger_error("Could not change privileges of local data folder '{$local_folder}{$base}'!", E_USER_ERROR);
+			trigger_error("Could not change privileges of local data folder '{$local_data}{$base}'!", E_USER_ERROR);
 		}
 	}
 }
 
-######################### VEP annotation data #########################
+######################### VEP cache #########################
 if ($build=="GRCh37")
 {
 	$annotation_folder = get_path("vep_data")."/cache/";
-	$local_annotation_folder = get_path("local_data")."/".basename(get_path("vep_data"))."/";
+	$local_annotation_folder = "{$local_data}/".basename(get_path("vep_data"))."/";
 
 	print "\n";
-	print "### VEP annotation data ###\n";
+	print "### VEP cache ###\n";
 	print "from: {$annotation_folder}\n";
 	print "to  : {$local_annotation_folder}\n";
 	print "\n";
@@ -169,7 +170,10 @@ if ($build=="GRCh37")
 		list($stdout, $stderr) = exec2("{$rsync} {$annotation_folder} {$local_annotation_folder}");
 		foreach(array_merge($stdout, $stderr) as $line)
 		{
-			print trim($line)."\n";
+			$line = trim($line);
+			if ($line=="") continue;
+			
+			print "  {$line}\n";
 		}
 		
 		touch("{$local_annotation_folder}/{$finished}");
@@ -180,14 +184,14 @@ if ($build=="GRCh37")
 	}
 }
 
-######################### GeneSplicer data #########################
+######################### VEP GeneSplicer data #########################
 if ($build=="GRCh37")
 {
 	$annotation_folder = dirname(get_path("vep"))."/GeneSplicer/human/";
-	$local_annotation_folder = get_path("local_data")."/GeneSplicer/";
+	$local_annotation_folder = "{$local_data}/GeneSplicer/";
 	
 	print "\n";
-	print "### GeneSplicer data ###\n";
+	print "### VEP GeneSplicer data ###\n";
 	print "from: {$annotation_folder}\n";
 	print "to  : {$local_annotation_folder}\n";
 	print "\n";
@@ -196,9 +200,70 @@ if ($build=="GRCh37")
 	list($stdout, $stderr) = exec2("{$rsync} {$annotation_folder} {$local_annotation_folder}");
 	foreach(array_merge($stdout, $stderr) as $line)
 	{
-		print trim($line)."\n";
+			$line = trim($line);
+			if ($line=="") continue;
+			
+			print "  {$line}\n";
+	}
+}
+
+
+######################### VEP annotation databases #########################
+if ($build=="GRCh37")
+{
+	$local_annotation_folder = "{$local_data}/ensembl-vep-dbs/";
+
+	//create local folder if missing
+	if (!file_exists($local_annotation_folder))
+	{
+		if (!mkdir($local_annotation_folder))
+		{
+			trigger_error("Could not create local data annotation folder '{$local_annotation_folder}'!", E_USER_ERROR);
+		}
+		if (!chmod($local_annotation_folder, 0777))
+		{
+			trigger_error("Could not change privileges of local annotation data folder '{$local_annotation_folder}'!", E_USER_ERROR);
+		}
 	}
 	
+	print "\n";
+	print "### VEP annotation databases ###\n";
+	print "to  : {$local_annotation_folder}\n";
+	print "\n";
+		
+	print "rsync-ing annotation databases...\n";
+	
+	$db_files = array("/dbs/CADD/whole_genome_SNVs.tsv.gz", "/dbs/CADD/InDels.tsv.gz", "/dbs/REVEL/revel_all_chromosomes.tsv.gz", "/dbs/fathmm-MKL/fathmm-MKL_Current.tab.gz", "/dbs/dbscSNV/dbscSNV1.1_GRCh37.txt.gz", "/dbs/gnomAD/gnomAD_genome_r2.1.vcf.gz", "/dbs/RepeatMasker/RepeatMasker.bed.gz", "/dbs/ClinVar/clinvar_20190123_converted.vcf.gz", "/dbs/phyloP/hg19.100way.phyloP100way.bw", "/dbs/OMIM/omim.bed.gz", "/dbs/HGMD/HGMD_PRO_2018_4_fixed.vcf.gz");
+	foreach($db_files as $db_file)
+	{
+		$source = $data_folder.$db_file;
+		$target = $local_annotation_folder.basename($db_file);
+		print "  rsync-ing database {$source}\n";
+		list($stdout, $stderr) = exec2("{$rsync} {$source} {$target}");
+		foreach(array_merge($stdout, $stderr) as $line)
+		{
+			$line = trim($line);
+			if ($line=="") continue;
+			
+			print "    {$line}\n";
+		}
+		
+		$source_index = $source.".tbi";
+		if (file_exists($source_index))
+		{
+			print "  rsync-ing database index {$source_index}\n";
+			
+			$target_index = $target.".tbi";
+			list($stdout, $stderr) = exec2("{$rsync} {$source_index} {$target_index}");
+			foreach(array_merge($stdout, $stderr) as $line)
+			{
+				$line = trim($line);
+				if ($line=="") continue;
+				
+				print "    {$line}\n";
+			}
+		}
+	}
 }
 
 ?>
