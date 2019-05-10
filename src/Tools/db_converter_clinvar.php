0c08a317e47cc4304c14695bacde05cff1a47891
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2019-05-10 15:30:37 +0200

    Updated ClinVar converter to fix non-conflicting interpretations

--- src/Tools/db_converter_clinvar.php
+++ src/Tools/db_converter_clinvar.php
@@ -7,7 +7,28 @@ function substr_clear($str, $start)
 	return trim(strtr(substr($str, $start), array("|"=>"[c]", ","=>"[c]")));
 }
 
+function cons2value($cons)
+{
+	if ($cons=="pathogenic") return 5;
+	if ($cons=="likely_pathogenic") return 4;
+	if ($cons=="uncertain_significance") return 3;
+	if ($cons=="likely_benign") return 2;
+	if ($cons=="benign") return 1;
+	trigger_error("Unknown consequence '$cons' in function 'cons2value'!", E_USER_ERROR);
+}
 
+function value2cons($value)
+{
+	if ($value<1 || $value>5)
+	{
+		trigger_error("Invalid consequence value '$value' in function 'value2cons'!", E_USER_ERROR);
+	}
+	if ($value>4.5) return "Pathogenic";
+	if ($value>3.5) return "Likely_pathogenic";
+	if ($value<0.5) return "Benign";
+	if ($value<1.5) return "Likely_benign";
+	return "Uncertain_significance";
+}
 //locale for iconv below
 setlocale(LC_ALL, 'de_DE.UTF8');
 
@@ -47,6 +68,8 @@ while(!feof($in))
 	//extract clinical significance and disease name (both for single variant and variant combinations, e.g. comp-het)
 	$infos = explode(";", $parts[7]);
 	$sig = "";
+	$sig_conf = "";
+	$replace_msg = "";
 	$dis = "";
 	$sig_inc = "";
 	$dis_inc = "";
@@ -56,6 +79,10 @@ while(!feof($in))
 		if (starts_with($info, "CLNSIG="))
 		{
 			$sig = substr_clear($info, 7);
+		}
+		if (starts_with($info, "CLNSIGCONF="))
+		{
+			$sig_conf = strtolower(substr_clear($info, 11));
 		}
 		if (starts_with($info, "CLNDN="))
 		{
@@ -68,6 +95,38 @@ while(!feof($in))
 		if (starts_with($info, "CLNDNINCL="))
 		{
 			$dis_inc = substr_clear($info, 10);
+		}
+	}
+	
+	//handle conflicting interpretations that are not conflicting
+	if ($sig_conf!="")
+	{
+		if (!(contains($sig_conf, "benign") && contains($sig_conf, "pathogenic")))
+		{
+			//calcualate the mean consequence
+			$cons_counts = array(1=>0, 2=>0, 3=>0, 4=>0, 5=>0);
+			$sig_conf = explode("%3b", $sig_conf);
+			foreach($sig_conf as $entry)
+			{
+				$entry = trim($entry);
+				$entry = substr($entry, 0, -1);
+				list($cons, $count) = explode("(", $entry);
+				$cons_counts[cons2value($cons)] += $count;
+			}
+			
+			$counts = [];
+			$avg = 0;
+			foreach($cons_counts as $cons=>$count)
+			{
+				$avg += $cons * $count;
+				if ($count!=0)
+				{
+					$counts[] = "{$count}x{$cons}";
+				}
+			}
+			$avg /= array_sum(array_values($cons_counts));
+			$sig = value2cons($avg);
+			$replace_msg = "(replaced_by_megSAP_based_on_classifications:".implode("[c]", $counts).")";
 		}
 	}
 	
@@ -101,7 +160,7 @@ while(!feof($in))
 	for($i=0; $i<count($sigs); ++$i)
 	{
 		$parts[2] = $accs[$i];	
-		$parts[7] = "DETAILS=".strtolower($sigs[$i])."[p]".iconv("utf-8","ascii//TRANSLIT", $diss[$i]);		
+		$parts[7] = "DETAILS=".strtolower($sigs[$i])."{$replace_msg}[p]".iconv("utf-8","ascii//TRANSLIT", $diss[$i]);		
 		print implode("\t", $parts)."\n";
 	}
 }
