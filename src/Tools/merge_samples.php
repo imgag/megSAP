6c769739ea0ea4c9bccc436cb47bad9e9ddc8970
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2019-05-20 10:00:56 +0200

    merge_samples: Now updates the NGSD as well.

--- src/Tools/merge_samples.php
+++ src/Tools/merge_samples.php
@@ -9,22 +9,29 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 //parse command line arguments
 $parser = new ToolBase("merge_samples", "Merges a processed sample into another processed sample.");
-$parser->addString("s1", "Sample folder that is merged into the second sample.", false);
-$parser->addString("s2", "Sample folder into which the first sample is merged.", false);
+$parser->addString("ps", "Processed sample to merged into the second sample.", false);
+$parser->addString("into", "Processed sample into which the first sample is merged.", false);
 extract($parser->parse($argv));
 
+//get processed sample infos
+$db_conn = DB::getInstance("NGSD");
+$info1 = get_processed_sample_info($ps);
+$info2 = get_processed_sample_info($into);
+
 //check input folders
-if (!is_dir($s1))
+$folder1 = $info1['ps_folder'];
+if (!is_dir($folder1))
 {
-	trigger_error("Sample folder '$s1' does not exist!", E_USER_ERROR);
+	trigger_error("Sample folder '$folder1' does not exist!", E_USER_ERROR);
 }
-if (!is_dir($s2))
+$folder2 = $info2['ps_folder'];
+if (!is_dir($folder2))
 {
-	trigger_error("Sample folder '$s2' does not exist!", E_USER_ERROR);
+	trigger_error("Sample folder '$folder2' does not exist!", E_USER_ERROR);
 }
 
 //make sure the "+merged_samples" folder exists
-$backup_folder = dirname($s1)."/+merged_samples";
+$backup_folder = dirname($folder1)."/+merged_samples";
 if (!file_exists($backup_folder))
 {
 	if (!mkdir($backup_folder))
@@ -34,25 +41,17 @@ if (!file_exists($backup_folder))
 }
 
 //move FASTQ files
-exec2("mv $s1/*.fastq.gz $s2/");
+exec2("mv $folder1/*.fastq.gz $folder2/");
 
 //move sample folder to backup
-exec2("mv $s1 $backup_folder/");
+exec2("mv $folder1 $backup_folder/");
 
-//remove detected variants for s1
-$name = strtr(basename($s1), array("Sample_"=>""));
-$db_conn = DB::getInstance("NGSD");
-$ps_id = get_processed_sample_id($db_conn, $name, false);
-if ($ps_id==-1)
-{
-	print "Skipping NGSD cleanup because sample '$name' was not found!\n";
-}
-else
-{
-	$db = DB::getInstance("NGSD");
-	$db->executeStmt("DELETE FROM detected_variant WHERE processed_sample_id='$ps_id'");
-	$db->executeStmt("DELETE FROM processed_sample_qc WHERE processed_sample_id='$ps_id'");
-}
+//NGSD: remove variants/qc for 'ps'
+$ps_id = $info1['ps_id'];
+$db->executeStmt("DELETE FROM detected_variant WHERE processed_sample_id='$ps_id'");
+$db->executeStmt("DELETE FROM processed_sample_qc WHERE processed_sample_id='$ps_id'");
 
+//NGSD: mark samples as merged
+$db->executeStmt("INSERT INTO `merged_processed_samples` (`processed_sample_id`, `merged_into`) VALUES ({$ps_id},".$info2['ps_id'].")");
 
 ?>
