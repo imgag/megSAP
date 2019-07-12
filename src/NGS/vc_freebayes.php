3dec962671f801b4383fd2317d46e77c4e3715f6
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2019-07-12 10:44:35 +0200

    vc_freebayes: increased the default base quality cutoff to 15 (FP base changes can influence the selected alleles in multi-allelic sites and lead to wrong genotyping)

--- src/NGS/vc_freebayes.php
+++ src/NGS/vc_freebayes.php
@@ -19,7 +19,7 @@ $parser->addInt("threads", "The maximum number of threads used.", true, 1);
 $parser->addString("build", "The genome build to use.", true, "GRCh37");
 $parser->addFloat("min_af", "Minimum allele frequency cutoff used for variant calling.", true, 0.15);
 $parser->addInt("min_mq", "Minimum mapping quality cutoff used for variant calling.", true, 1);
-$parser->addInt("min_bq", "Minimum base quality cutoff used for variant calling.", true, 10);
+$parser->addInt("min_bq", "Minimum base quality cutoff used for variant calling.", true, 15);
 $parser->addFlag("no_ploidy", "Use freebayes parameter -K, i.e. output all alleles which pass input filters, regardles of genotyping outcome or model.");
 $parser->addFlag("no_bias", "Use freebayes parameter -V, i.e. ignore strand bias and read end distance bias.");
 extract($parser->parse($argv));
@@ -56,7 +56,7 @@ if ($no_bias)
 }
 $args[] = "--min-alternate-fraction $min_af";
 $args[] = "--min-mapping-quality $min_mq";
-$args[] = "--min-base-quality $min_bq"; //max 10% error propbability
+$args[] = "--min-base-quality $min_bq";
 $args[] = "--min-alternate-qsum 90"; //At least 3 good observations
 $args[] = "-f $genome";
 $args[] = "-b ".implode(" ", $bam);
