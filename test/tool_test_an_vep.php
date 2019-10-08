256cbde5da36c0ad38b772cdec083e9ea9e9ed99
leonschuetz <leon.schuetz@med.uni-tuebingen.de>
2019-10-08 13:21:51 +0200

    an_vep: added NGSD annotation by VCF file (VcfAnnotateFromVcf)
    vcf2gsvar: removed NGSD annotation
    
    annotate: added NGSD annotation from VCF file
    
    somatic_dna: added NGSD annotation from VCF file
    
    somatic_rna.php: added NGSD annotation from VCF file

--- test/tool_test_an_vep.php
+++ test/tool_test_an_vep.php
@@ -7,7 +7,7 @@ start_test($name);
 
 //standard
 $out_file1 = output_folder().$name."_out1.vcf";
-check_exec("php ".src_folder()."/NGS/{$name}.php -in ".data_folder().$name."_in1.vcf -out $out_file1 --log ".output_folder().$name."_out1.log");
+check_exec("php ".src_folder()."/NGS/{$name}.php -test -in ".data_folder().$name."_in1.vcf -out $out_file1 --log ".output_folder().$name."_out1.log");
 remove_lines_containing($out_file1, array("##VEP=\"v"));
 check_file($out_file1, data_folder().$name."_out1.vcf", true);
 
@@ -16,6 +16,24 @@ $out_file2 = output_folder().$name."_out2.vcf";
 check_exec("php ".src_folder()."/NGS/{$name}.php -in ".data_folder().$name."_in_empty.vcf -out $out_file2 --log ".output_folder().$name."_out2.log");
 remove_lines_containing($out_file2, array("##VEP=\"v"));
 check_file($out_file2, data_folder().$name."_out2.vcf", true);
+
+//somatic
+$out_file3 = output_folder().$name."_out3.vcf";
+check_exec("php ".src_folder()."/NGS/{$name}.php -test -somatic -in ".data_folder().$name."_in1.vcf -out $out_file3 --log ".output_folder().$name."_out3.log");
+remove_lines_containing($out_file3, array("##VEP=\"v"));
+check_file($out_file3, data_folder().$name."_out3.vcf", true);
+
+//NA12878_38 head (disease group test)
+$out_file4 = output_folder().$name."_out4.vcf";
+check_exec("php ".src_folder()."/NGS/{$name}.php -test -in ".data_folder().$name."_in3.vcf -out $out_file4 --log ".output_folder().$name."_out4.log");
+remove_lines_containing($out_file4, array("##VEP=\"v"));
+check_file($out_file4, data_folder().$name."_out4.vcf", true);
+
+//NA12878_38 head zipped (disease group test)
+$out_file5 = output_folder().$name."_out5.vcf";
+check_exec("php ".src_folder()."/NGS/{$name}.php -test -in ".data_folder().$name."_in4.vcf.gz -out $out_file5 --log ".output_folder().$name."_out5.log");
+remove_lines_containing($out_file5, array("##VEP=\"v"));
+check_file($out_file5, data_folder().$name."_out5.vcf", true);
 
 end_test();
 
