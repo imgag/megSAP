719b887de063cc7ca2ac66189a4f4d8514e3b235
Axel Gschwind <axel.gschwind@t-online.de>
2019-11-22 13:48:01 +0100

    tool_test_bedp2somatic.php: Corrected order of file comparison to make sure no temp files are stored in test data folder

--- test/tool_test_bedpe2somatic.php
+++ test/tool_test_bedpe2somatic.php
@@ -9,13 +9,13 @@ $out = output_folder().$name."_out1.bedpe";
 $in = data_folder().$name."_in1.bedpe";
 $ref = data_folder().$name."_ref1.bedpe";
 check_exec("php ".src_folder()."/Tools/bedpe2somatic.php -in $in -out $out -tid DX000002_01 -nid DX000001_01");
-check_file($ref, $out, true);
+check_file($out, $ref, true);
 
 $out = output_folder().$name."_out2.bedpe";
 $in = data_folder().$name."_in2.bedpe";
 $ref = data_folder().$name."_ref2.bedpe";
 check_exec("php ".src_folder()."/Tools/bedpe2somatic.php -in $in -out $out -tid DX000002_01 -nid DX000001_01");
-check_file($ref, $out, true);
+check_file($out, $ref, true);
 
 
 end_test();
