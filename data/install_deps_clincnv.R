7a16a0af1f42ce54ee6b8246c462267282016935
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2019-12-11 10:49:13 +0100

    Fixed installation instruction problems (#38)

--- data/install_deps_clincnv.R
+++ data/install_deps_clincnv.R
@@ -1,6 +1,6 @@
 install.packages("optparse", repos="https://cran.rstudio.com")
 install.packages("robustbase", repos="https://cran.rstudio.com")
-install.packages("mass", repos="https://cran.rstudio.com")
+install.packages("MASS", repos="https://cran.rstudio.com")
 install.packages("data.table", repos="https://cran.rstudio.com")
 install.packages("foreach", repos="https://cran.rstudio.com")
 install.packages("doParallel", repos="https://cran.rstudio.com")
@@ -8,7 +8,7 @@ install.packages("mclust", repos="https://cran.rstudio.com")
 install.packages("R.utils", repos="https://cran.rstudio.com")
 install.packages("RColorBrewer", repos="https://cran.rstudio.com")
 
-#Party is not available for R 3.4, which is installed on Ubuntu 18.04. So we install the old version manually
+#Party is no longer available for R 3.4, which is installed on Ubuntu 18.04. So we install the old version manually
 library("devtools")
 install_version("mvtnorm", version = "1.0-8", repos = "http://cran.us.r-project.org")
 install_version("coin", version = "1.0-8", repos = "http://cran.us.r-project.org")
