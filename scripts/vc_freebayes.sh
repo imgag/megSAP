1e7aba15cd2145681ce5fdfb1964b6ab0747e694
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2020-03-06 14:56:20 +0100

    Improved vc helper script

--- scripts/vc_freebayes.sh
+++ scripts/vc_freebayes.sh
@@ -3,10 +3,11 @@ if [ $# -lt 2 ]
 then
   echo "Usage:"
   echo " vc_freebayes.sh [chr:start-end] [bam] <options>"
-  echo "Options:"
-  echo " --min-alternate-fraction [0.2]"
-  echo " --min-mapping-quality [1]"
+  echo "Default options for germline calling:"
+  echo " --min-alternate-fraction 0.1 --min-mapping-quality 1 --min-base-quality 15 --min-alternate-qsum 90"
   exit
 fi
 
-/mnt/share/opt/freebayes-1.2.0/bin/freebayes -b $2 -r $1 -f /mnt/share/data/genomes/GRCh37.fa --min-base-quality 20 --min-alternate-qsum 90 ${@:3}
+/mnt/share/opt/freebayes-1.2.0/bin/freebayes -b $2 -r $1 -f /mnt/share/data/genomes/GRCh37.fa ${@:3}
+
+
