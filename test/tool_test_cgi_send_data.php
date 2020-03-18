aa0f64c6351fffab87df5ddbccd020d45b335f1a
Axel Gschwind <axel.gschwind@t-online.de>
2020-03-18 16:08:17 +0100

    cgi_send_data.php: genes to be sent are now taken from gene_info column (ClinCNV sorted out few genes in gene column due to low GC content, but we want a statement for all genes)

--- test/tool_test_cgi_send_data.php
+++ test/tool_test_cgi_send_data.php
@@ -13,8 +13,7 @@ $out_zip = output_folder() ."/cgi_results1.zip";
 $out_dir1 = output_folder() ."/out1/";
 $out_dir2 = output_folder() ."/out2/";
 $out_dir3 = output_folder() ."/out3/";
-$out_dir4 = output_folder() ."/out4/";
-exec2("mkdir $out_dir1 $out_dir2 $out_dir3 $out_dir4");
+exec2("mkdir $out_dir1 $out_dir2 $out_dir3");
 
 
 //Case 1: input is vcf.gz file
@@ -26,31 +25,18 @@ check_column_exists($out_dir1."mutation_analysis.tsv",$col_names_mut);
 check_file_exists($out_dir1."drug_prescription.tsv");
 check_file_exists($out_dir1."drug_prescription_bioactivities.tsv");
 
-
 //Case 2: input file is .vcf file (output must be the same as if vcf.gz as input)
 $in_file_snv2 = data_folder() . "/cgisenddata_in2_var_annotated.vcf";
 check_exec("php ".src_folder()."/NGS/cgi_send_data.php -mutations $in_file_snv2 -cancertype SK -out $out_zip");
 exec2("unzip -n $out_zip -d " . $out_dir2 );
 check_column_exists($out_dir2."mutation_analysis.tsv",$col_names_mut);
 
-
-//Case 3: input file .vcf and ClinCNV CNV
-$in_file_snv3 = data_folder() . "/cgisenddata_in3_var_annotated.vcf";
+//Case 3: Upload CNV file with target region set
 $in_file_cnv3 = data_folder() . "/cgisenddata_in3_clincnv.tsv";
-check_exec("php ".src_folder()."/NGS/cgi_send_data.php -mutations $in_file_snv3 -cnas $in_file_cnv3 -cancertype SK -out $out_zip");
-exec2("unzip -n $out_zip -d " . $out_dir3 );
-check_column_exists($out_dir3."mutation_analysis.tsv",$col_names_mut);
-check_tsv_file($out_dir3."cna_analysis.tsv",data_folder()."cgisenddata_ref3_cgi_cnv_analysis.tsv");
-check_file_exists($out_dir3."drug_prescription.tsv");
-check_file_exists($out_dir3."drug_prescription_bioactivities.tsv");
-
-
-//Case 4: Upload CNV file with target region set
-$in_file_cnv4 = data_folder() . "/cgisenddata_in3_clincnv.tsv";
 $target_region = data_folder() . "/cgisenddata_target_region_genes.txt";
-check_exec("php ".src_folder()."/NGS/cgi_send_data.php -cnas $in_file_cnv4 -cancertype SK -out $out_zip -t_region $target_region",true);
-exec2("unzip -n $out_zip -d " . $out_dir4 );
-check_tsv_file($out_dir4."cna_analysis.tsv",data_folder()."cgisenddata_ref4_cgi_cnv_analysis.tsv");
+check_exec("php ".src_folder()."/NGS/cgi_send_data.php -cnas $in_file_cnv3 -cancertype SK -out $out_zip -t_region $target_region",true);
+exec2("unzip -n $out_zip -d " . $out_dir3 );
+check_tsv_file($out_dir3."cna_analysis.tsv",data_folder()."cgisenddata_ref3_cgi_cnv_analysis.tsv");
 
 
 end_test();
