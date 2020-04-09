2e628b8fc9ae8bb5f28471ec09522a64454e2c53
Leon Schuetz <Leon.Schuetz@med.uni-tuebingen.de>
2020-04-09 14:47:20 +0200

    src/NGS/create_circos_plot.php: changed comments

--- src/NGS/create_circos_plot.php
+++ src/NGS/create_circos_plot.php
@@ -13,7 +13,7 @@ $parser->addString("folder", "Analysis data folder.", false);
 $parser->addString("name", "Base file name, typically the processed sample ID (e.g. 'GS120001_01').", false);
 $parser->addString("build", "The genome build to use. The genome must be indexed for BWA!", true, "GRCh37");
 
-# optional
+// optional
 $parser->addString("cnv_min_ll", "Minimum loglikelyhood for CNVs to be shown in the plot.", true, 20);
 $parser->addString("cnv_min_nor", "Minimum number of regions for CNVs to be shown in the plot.", true, 3);
 $parser->addString("cnv_max_af", "Maximum IMGAG allele frequency for CNVs to be shown in the plot.", true, 0.5);
@@ -22,7 +22,7 @@ $parser->addString("roh_min_length", "Minimimal ROH length (in kb) for ROHs to b
 
 extract($parser->parse($argv));
 
-# get Cicos config and genome files 
+// get Cicos config and genome files 
 $karyotype_file = repository_basedir() . "/data/misc/circos/karyotype.human.$build.txt";
 if (!file_exists($karyotype_file)) 
 {
@@ -44,7 +44,7 @@ if (!file_exists($circos_housekeeping_file))
     trigger_error("No Circos config file found at \"$circos_housekeeping_file\"!", E_USER_ERROR);
 }
 
-# get telomere file
+// get telomere file
 $telomere_file = repository_basedir() . "/data/misc/centromer_telomer_hg19.bed";
 if (!file_exists($telomere_file)) 
 {
@@ -52,8 +52,8 @@ if (!file_exists($telomere_file))
 }
 
 
-# preprocess data
-# parse CN file and generate temp file for circos plot
+// preprocess data
+// parse CN file and generate temp file for circos plot
 $seg_file = "$folder/${name}_cnvs_clincnv.seg";
 
 $cn_temp_file = $parser->tempFile("_cn.seg");
@@ -92,7 +92,7 @@ else
     return  "Could not open file $seg_file.";
 }
 
-# parse CNVs file and generate temp file for circos plot
+// parse CNVs file and generate temp file for circos plot
 $cnv_file = "$folder/${name}_cnvs_clincnv.tsv";
 
 $cnv_temp_file_del = $parser->tempFile("_cnv_del.tsv");
@@ -121,18 +121,18 @@ $output_fh_del = fopen2($cnv_temp_file_del, "w");
 $output_fh_dup = fopen2($cnv_temp_file_dup, "w");
 for ($row_idx=0; $row_idx < $cnv_matrix->rows(); $row_idx++) 
 { 
-    # filter CNV
-    # loglikelihood
+    // filter CNV
+    // loglikelihood
     if ((int) $cnv_matrix->get($row_idx, $ll_idx) < $cnv_min_ll) continue;
-    # no_of_regions
+    // no_of_regions
     if ((int) $cnv_matrix->get($row_idx, $nor_idx) < $cnv_min_nor) continue;
-    # overlap af (imgag)
+    // overlap af (imgag)
     if ((float) $cnv_matrix->get($row_idx, $overlap_af_idx) > $cnv_max_af) continue;
-    # CNV size
+    // CNV size
     if ((float) $cnv_matrix->get($row_idx, $length_idx) < $cnv_min_length) continue;
 
 
-    # write modified cnv files to temp
+    // write modified cnv files to temp
     if ((int) $cnv_matrix->get($row_idx, $cn_idx) < 2) 
     {
         fwrite($output_fh_del, $cnv_matrix->get($row_idx, $chr_idx)."\t".$cnv_matrix->get($row_idx, $start_idx)."\t".$cnv_matrix->get($row_idx, $end_idx)."\n");
@@ -147,7 +147,7 @@ fclose($output_fh_del);
 fclose($output_fh_dup);
 
 
-# parse ROHs file and generate temp file for circos plot
+// parse ROHs file and generate temp file for circos plot
 $roh_file = "$folder/${name}_rohs.tsv";
 
 $roh_temp_file = $parser->tempFile("_roh.tsv");
@@ -156,7 +156,7 @@ if (!file_exists($roh_file))
 {
     trigger_error("WARNING: No ROH file found!", E_USER_WARNING);
     
-    # create empty temp file
+    // create empty temp file
     touch($roh_temp_file);
 }
 else
@@ -168,16 +168,16 @@ else
     {
         while (($buffer = fgets($input_fh)) !== FALSE) 
         {
-            # skip comments and header
+            // skip comments and header
             if (starts_with($buffer, "#")) continue;
             $row = explode("\t", $buffer);
 
-            # skip small ROHs 
+            // skip small ROHs 
             if ((float) $row[5] < $roh_min_length) continue;
 
             $cn = min(4.0, (float) $row[5]);
 
-            # write modified roh file to temp
+            // write modified roh file to temp
             fwrite($output_fh, $row[0] . "\t" . $row[1] . "\t" . $row[2] . "\n");
         }
         fclose($input_fh);
@@ -193,7 +193,7 @@ else
 
 
 
-# parse BAFs file and generate temp file for circos plot
+// parse BAFs file and generate temp file for circos plot
 $baf_file = "$folder/${name}_bafs.igv";
 
 $baf_temp_file = $parser->tempFile("_bafs.tsv");
@@ -202,7 +202,7 @@ if (!file_exists($baf_file))
 {
     trigger_error("WARNING: No BAF file found!", E_USER_WARNING);
 
-    # create empty temp file
+    // create empty temp file
     touch($baf_temp_file);
 }
 else
@@ -214,12 +214,12 @@ else
     {
         while (($buffer = fgets($input_fh)) !== FALSE) 
         {
-            # skip comments and header
+            // skip comments and header
             if (starts_with($buffer, "#")) continue;
             if (starts_with($buffer, "Chromosome\tStart\tEnd")) continue;
             $row = explode("\t", $buffer);
 
-            # write modified baf file to temp
+            // write modified baf file to temp
             fwrite($output_fh, $row[0] . "\t" . $row[1] . "\t" . $row[2] . "\t" . $row[4] . "\n");
         }
         fclose($input_fh);
@@ -234,13 +234,13 @@ else
 
 
 
-# create dummy file to add sample name to plot
+// create dummy file to add sample name to plot
 $sample_label = $parser->tempFile("_sample_label.txt");
 $output_fh = fopen2($sample_label, "w");
 fwrite($output_fh, "chr1\t1\t1000000\t$name\n");
 fclose($output_fh);
 
-# create modified Circos config file
+// create modified Circos config file
 $file_names = array();
 $file_names["[OUTPUT_FOLDER]"] = $folder;
 $file_names["[PNG_OUTPUT]"] = "${name}_circos.png";
@@ -256,7 +256,7 @@ $file_names["[SAMPLE_LABEL]"] = $sample_label;
 $file_names["[LABEL_FOLDER]"] = repository_basedir()."/data/misc/circos";
 $file_names["[HOUSEKEEPING_FILE]"] = $circos_housekeeping_file;
 
-# parse circos template file and replace file names
+// parse circos template file and replace file names
 $circos_config_file = "$folder/${name}_circos_config.conf";
 
 $input_fh = fopen2($circos_template_file, "r");
@@ -271,10 +271,10 @@ if ($input_fh)
 {
     while (($buffer = fgets($input_fh)) !== FALSE) 
     {
-        # replace file names
+        // replace file names
         $modified_line = strtr($buffer, $file_names);
 
-        # write modified baf file to temp
+        // write modified baf file to temp
         fwrite($output_fh, $modified_line);
     }
     fclose($input_fh);
@@ -287,5 +287,5 @@ else
 }
 
 
-# create Circos plot
+// create Circos plot
 $parser->exec(get_path("circos"), "-conf $circos_config_file", true);
