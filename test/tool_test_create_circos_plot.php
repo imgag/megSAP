24e007d762905feda4dd3b1e26846b3aa8848cf8
leonschuetz <leon.schuetz@med.uni-tuebingen.de>
2020-04-27 12:33:49 +0200

    test/tool_test_create_circos_plot.php: removed CnvHunter test
    test/data/create_circos_plot*: removed files for CnvHunter test
    
    test/data_amplicon/*: removed SV calling from pipeline test
    
    test/data_chrx/*: changed type to WES and updated test

--- test/tool_test_create_circos_plot.php
+++ test/tool_test_create_circos_plot.php
@@ -20,20 +20,6 @@ copy(data_folder()."create_circos_plot_in_rohs.tsv", $roh_file);
 check_exec("php ".src_folder()."/NGS/".$name.".php -folder ".output_folder()." -name clincnvTest");
 check_file(output_folder()."clincnvTest_circos.png", data_folder().$name."_out_circos_clincnv.png");
 
-//test 2: CnvHunter and modified parameter
-// copy data 
-$baf_file = output_folder()."cnvHunterTest_bafs.igv";
-$seg_file = output_folder()."cnvHunterTest_cnvs.seg";
-$cnv_file = output_folder()."cnvHunterTest_cnvs.tsv";
-$roh_file = output_folder()."cnvHunterTest_rohs.tsv";
-copy(data_folder()."create_circos_plot_in_baf.igv", $baf_file);
-copy(data_folder()."create_circos_plot_in_seg_cnvHunter.seg", $seg_file);
-copy(data_folder()."create_circos_plot_in_cnv_cnvHunter.tsv", $cnv_file);
-copy(data_folder()."create_circos_plot_in_rohs.tsv", $roh_file);
-// create plot
-check_exec("php ".src_folder()."/NGS/".$name.".php -folder ".output_folder()." -name cnvHunterTest -cn_offset 0 -roh_min_length 100");
-check_file(output_folder()."cnvHunterTest_circos.png", data_folder().$name."_out_circos_cnvHunter.png");
-
 end_test();
 
 ?>
