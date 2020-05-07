66e7b5320107057fdcb64d2eb7e57d31587a9b87
Axel Gschwind <axel.gschwind@t-online.de>
2020-05-07 16:48:07 +0200

    corrected varscan2 url in download script

--- data/download_tools_somatic.sh
+++ data/download_tools_somatic.sh
@@ -7,8 +7,8 @@ cd $folder
 
 #download varscan2
 mkdir VarScan.v2.4.4
-wget https://github.com/dkoboldt/varscan/blob/master/VarScan.v2.4.4.jar -O VarScan.v2.4.4/VarScan.v2.4.4.jar
-cd $folder
+wget https://github.com/dkoboldt/varscan/raw/master/VarScan.v2.4.4.jar -O VarScan.v2.4.4/VarScan.v2.4.4.jar
+
 
 #download strelka2
 wget https://github.com/Illumina/strelka/releases/download/v2.9.10/strelka-2.9.10.centos6_x86_64.tar.bz2
