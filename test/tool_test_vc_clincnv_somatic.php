69d74148a11065c9d6bae0b3b1309a108a06fb7d
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2020-05-18 10:59:27 +0200

    kasp_check: added output for samples not in NGSD; fixed several tests

--- test/tool_test_vc_clincnv_somatic.php
+++ test/tool_test_vc_clincnv_somatic.php
@@ -44,7 +44,15 @@ $n_cov_off = $cov_folder_n_off . "DX000015_02.cov";
 
 check_exec("php ".src_folder()."/NGS/{$name}.php -t_id DX000015_01 -n_id DX000015_02 -cov_folder_n $cov_folder_n -cov_folder_t $cov_folder_t -cohort_folder $cohort_folder2 -cov_pairs $t_n_pair_file -out $out_file -t_cov $t_cov -n_cov $n_cov -bed $bed_file -system $system_file -t_cov_off $t_cov_off -n_cov_off $n_cov_off -cov_folder_n_off $cov_folder_n_off -cov_folder_t_off $cov_folder_t_off -bed_off $bed_file_off -baf_folder $baf_folder");
 
-check_file($out_file, data_folder().$name."_out.tsv");
+// results depend on the availability of the NGSD 
+if (db_is_enabled("NGSD"))
+{
+	check_file($out_file, data_folder().$name."_out.tsv");
+}
+else
+{
+	check_file($out_file1, data_folder().$name."_out_noNGSD.tsv");
+}
 
 check_file_exists(output_folder()."/DX000015_01-DX000015_02_clonality.png");
 check_file_exists(output_folder()."/DX000015_01-DX000015_02_clonalityBarplot.png");
