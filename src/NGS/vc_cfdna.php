12c338dfd7c876bf3a54f1ad67309e34dd8c6a96
Jakob Admard <jakob.admard@med.uni-tuebingen.de>
2020-07-28 09:28:34 +0200

    add genome build option

--- src/NGS/vc_cfdna.php
+++ src/NGS/vc_cfdna.php
@@ -6,6 +6,7 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 $parser = new ToolBase("vc_cfdna", "cfDNA mutation caller.");
 $parser->addInfile("bam", "Input BAM file, with deduplicated alignments and DP tags.", false);
 $parser->addInfile("target", "Target region BED file.", false);
+$parser->addString("build", "Reference genome build.", false);
 $parser->addOutfile("vcf", "Variant call output as VCF file.", true);
 $parser->addOutfile("tsv", "Variant call output as TSV file.", true);
 $parser->addOutfile("mrd", "MRD probability output file.", true);
@@ -13,5 +14,7 @@ $parser->addInfile("model", "Error model parameters.", true);
 $parser->addInfile("model_region", "Target region to use for parameter estimation.", true);
 
 extract($parser->parse($argv));
+
+$genome = genome_fasta($build);
 
 ?>
