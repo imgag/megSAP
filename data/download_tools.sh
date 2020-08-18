06462e54a4eda6f93b0bb640e314ea1a0db85fce
Dominic Boceck <dboceck@gmail.com>
2020-08-18 16:56:20 +0200

    Update download_tools.sh
    Change AIdiva version to contain some bugfixes (otherwise the an_vep tests of megSAP will fail)

--- data/download_tools.sh
+++ data/download_tools.sh
@@ -68,12 +68,12 @@ cd $folder
 git clone https://github.com/imgag/AIdiva.git
 cd AIdiva
 git fetch && git fetch --tags
-git checkout 0.1
+git checkout 0.2
 cd data
 wget -c https://download.imgag.de/ahboced1/AIdiva_pretrained_models/rf_model_inframe_indel_scikit0-19-1.pkl
 wget -c https://download.imgag.de/ahboced1/AIdiva_pretrained_models/rf_model_snp_scikit0-19-1.pkl
 cd ..
-mv AIdiva AIdiva-0.1
+mv AIdiva AIdiva-0.2
 
 #download and build VEP
 cd $root
