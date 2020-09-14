50db2f2d0299c0fe5204b6698b527bd785a5411f
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2020-09-14 10:10:25 +0200

    Updated path of production megSAP installation

--- src/NGS/copy_sample.php
+++ src/NGS/copy_sample.php
@@ -4,7 +4,7 @@
 */
 
 require_once(dirname($_SERVER['SCRIPT_FILENAME'])."/../Common/all.php");
-$repo_folder = "/mnt/users/all/megSAP"; //fixed absolute path to make the tests work for all users
+$repo_folder = "/mnt/users/bioinf/megSAP"; //fixed absolute path to make the tests work for all users
 
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
