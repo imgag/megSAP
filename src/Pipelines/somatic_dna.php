f94e07e9870b2376c19f53fa42df82fb75651f7c
Tim Stohn <ahstoht1@SRV018.img.med.uni-tuebingen.de>
2020-12-10 14:49:10 +0100

    - calcualte BAF file for somatic tumor only with baf_germline.php

--- src/Pipelines/somatic_dna.php
+++ src/Pipelines/somatic_dna.php
@@ -402,6 +402,16 @@ if (in_array("vc", $steps))
 			$parser->execTool("NGS/baf_somatic.php", implode(" ", $baf_args));
 		}
 	}
+	else
+	{
+		//create b-allele frequency file
+		$params = array();
+		$params[] = "-vcf {$variants}";
+		$params[] = "-bam {$t_bam}";
+		$params[] = "-out {$ballele}";
+		$params[] = "-build ".$sys['build'];
+		$parser->execTool("NGS/baf_germline.php", implode(" ", $params));
+	}
 }
 
 //Viral sequences alignment
