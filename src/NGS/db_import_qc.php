c8b41ea040b1ffb165db557741cb49f0cce0f12d
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2021-02-09 08:03:26 +0100

    db_import_qc.php no longer needs the qcML OBO file

--- src/NGS/db_import_qc.php
+++ src/NGS/db_import_qc.php
@@ -8,56 +8,45 @@ require_once(dirname($_SERVER['SCRIPT_FILENAME'])."/../Common/all.php");
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 $parser = new ToolBase("db_import_qc", "Imports QC terms to NGSD.");
-$parser->addString("id", "Processing ID (e.g. GS000123_01).", false);
+$parser->addString("id", "Processes sample identifier (e.g. GS000123_01).", false);
 $parser->addInfileArray("files", "qcML files to import.", false, true);
 //optional
 $parser->addEnum("db",  "Database to connect to.", true, db_names(), "NGSD");
 $parser->addFlag("force", "Overwrites already existing DB entries instead of throwing an error.");
-$parser->addInt("min_depth", "Minimum average read depth required.", true, "40");
-$skip_qual_parameters = array();
-$parser->addString("skip_parameters", "Comma-separated list of quality parameters that are not inserted into the Database.", true, implode(",", $skip_qual_parameters));
 extract($parser->parse($argv));
-
-//load valid term list from OBO file
-load_qc_terms();
-$terms = $GLOBALS["qcml"];
-
-// Terms that are not uploaded
-$skip_parameters = explode(",", $skip_parameters);
 
 //check pid format
 if(!preg_match("/^([A-Za-z0-9]{4,})_(\d{2})$/", $id, $matches))
 {
 	trigger_error("'$id' is not a valid processing ID!", E_USER_ERROR);
 }
-$sid = $matches[1];
-$pid = (int)$matches[2];
+$sample = $matches[1];
+$process_id = (int)$matches[2];
 
 //establish database connection
 $db = DB::getInstance($db);
 
 // check if this PID exists in the DB
-$hash = $db->prepare("SELECT s.id as sid, ps.id as psid FROM sample as s, processed_sample as ps WHERE s.id = ps.sample_id and s.name = :sid and ps.process_id = :pid");
-$db->bind($hash, 'sid', $sid);
-$db->bind($hash, 'pid', $pid);
+$hash = $db->prepare("SELECT s.id as sid, ps.id as psid FROM sample as s, processed_sample as ps WHERE s.id = ps.sample_id and s.name = :sample and ps.process_id = :process_id");
+$db->bind($hash, 'sample', $sample);
+$db->bind($hash, 'process_id', $process_id);
 $db->execute($hash, true); 
 $result = $db->fetch($hash); 
 $db->unsetStmt($hash);
 if(count($result) != 1)
 {
-	trigger_error("PID '$id' not found in DB. Sample and Processed Sample entered to DB?", E_USER_ERROR);
+	trigger_error("Processed sample '$id' not found in DB. Sample and Processed Sample entered to DB?", E_USER_ERROR);
 }
 $psid = $result[0]['psid'];
 
 // check if QC parameters were already imported for this pid
-$result = $db->executeQuery("SELECT count(id) FROM processed_sample_qc WHERE processed_sample_id='$psid'");
-$count_old = $result[0]['count(id)'];
-$parser->log("Found $count_old QC terms for PID '$id' already in DB.");
+$count_old =  $db->getValue("SELECT count(id) FROM processed_sample_qc WHERE processed_sample_id='$psid'");
+$parser->log("Found $count_old QC terms for processed sample '$id' already in DB.");
 
 //throw error if not forced to overwrite and terms already exist
 if($count_old!=0 && !$force)
 {
-	trigger_error("QC terms were already imported for PID '$id'. Use '-force' to overwrite them.", E_USER_ERROR);
+	trigger_error("QC terms were already imported for processed sample '$id'. Use '-force' to overwrite them.", E_USER_ERROR);
 }
 
 //remove old terms
@@ -79,27 +68,25 @@ foreach($files as $file)
 		$key = $entry->getAttribute('accession');
 		$value = $entry->getAttribute('value');
 
-		//check term exists in the OBO file
-		if (!isset($terms[$key]))
+		//check term exists in the NGSD
+		$type = $db->getValue("SELECT type FROM qc_terms WHERE qcml_id='$key'", "");
+		if ($type=="")
 		{
-			trigger_error("qcML term '$key' not found in OBO file!", E_USER_ERROR);
-		}
-		$term = $terms[$key];
-		
-		//skip numeric terms that are not available
-		if ($term["type"]!="string" && starts_with($value, "n/a"))
-		{
+			trigger_error("QC term '$key' not found in NGSD > skipped!", E_USER_NOTICE);
 			continue;
 		}
 		
-		//skip terms that should be skipped
-		if(in_array($key, $skip_parameters)) continue;
-		
+		//skip numeric terms that are not available
+		if ($type!="string" && starts_with($value, "n/a"))
+		{
+			continue;
+		}
+				
 		//error if term is found several times
 		if(isset($qc_par[$key])) trigger_error("Found QC term '$key' more than once!", E_USER_ERROR);
 		
 		//check that value is of the correct type
-		switch($term["type"])
+		switch($type)
 		{
 			case "int":
 				if(!is_int((int)$value))
@@ -131,31 +118,20 @@ foreach($files as $file)
 		$qc_par[$key] = $value;
 	}
 }
-$parser->log("Found ".count($qc_par)." QC terms for PID '$id' in log files.");
+$parser->log("Found ".count($qc_par)." QC terms for processed sample '$id'.");
 
 // insert QC terms into the DB
-$hash1 = $db->prepare("SELECT id FROM qc_terms WHERE qcml_id = :0");
-$hash2 = $db->prepare("INSERT INTO processed_sample_qc (processed_sample_id, qc_terms_id, value) VALUES (:0, :1, :2)");
+$hash = $db->prepare("INSERT INTO processed_sample_qc (processed_sample_id, qc_terms_id, value) VALUES (:0, :1, :2)");
 $db->beginTransaction();
 foreach($qc_par as $key => $value)
 {
-	//check if term is in NGSD
-	$db->bind($hash1, "0", $key);
-	$db->execute($hash1, true);
-	$res1 = $db->fetch($hash1);
-	if (count($res1)==0)
-	{
-		trigger_error("QC term '$key' it found in NGSD, thus not imported!", E_USER_NOTICE);
-		continue;
-	}
-	
-	$db->bind($hash2, "0", $psid);
-	$db->bind($hash2, "1", $res1[0]['id']);
-	$db->bind($hash2, "2", $value);
-	$db->execute($hash2, true);
+	$db->bind($hash, "0", $psid);
+	$qc_id = $db->getValue("SELECT id FROM qc_terms WHERE qcml_id='$key'");
+	$db->bind($hash, "1", $qc_id);
+	$db->bind($hash, "2", $value);
+	$db->execute($hash, true);
 }
 $db->endTransaction();
-$db->unsetStmt($hash1);
-$db->unsetStmt($hash2);
+$db->unsetStmt($hash);
 
 ?>
