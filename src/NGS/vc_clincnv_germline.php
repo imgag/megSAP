8232ed2daec5a022b55f4da45031b6b9c78237c7
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2021-03-03 15:44:14 +0100

    vc_clincnv_germline: Improved error handling

--- src/NGS/vc_clincnv_germline.php
+++ src/NGS/vc_clincnv_germline.php
@@ -212,7 +212,7 @@ $cov_files[] = $cov;
 $cov_files = array_unique(array_map("realpath", $cov_files));
 if (count($cov_files)<$cov_min)
 {
-	generate_empty_cnv_file($out, $command, "", $ps_name, "Only ".count($cov_files)." coverage files found in folder '{$cov_folder}'", $tumor_only);
+	generate_empty_cnv_file($out, $command, [], $ps_name, "Only ".count($cov_files)." coverage files found in folder '{$cov_folder}'", $tumor_only);
 	trigger_error("CNV calling skipped. Only ".count($cov_files)." coverage files found in folder '$cov_folder'. At least {$cov_min} files are needed!", E_USER_ERROR);
 }
 
@@ -469,7 +469,7 @@ if(file_exists("{$out_folder}/{$clinCNV_result_folder}/{$ps_name}/{$ps_name}_cnv
 else
 {
 	//ClinVAR did not generate CNV file
-	generate_empty_cnv_file($out, $command, $stdout, $ps_name, implode("; ",$stderr), $tumor_only);
+	generate_empty_cnv_file($out, $command, $stdout, $ps_name, "ClinCNV did not produce an output file!", $tumor_only);
 }
 
 ?>
