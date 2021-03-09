4597e7b898081bb7bc7f0c12f21b3ffb16d4764b
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2021-03-09 07:05:14 +0100

    Fixed handling of empty target region in processing system

--- src/Common/genomics.php
+++ src/Common/genomics.php
@@ -406,7 +406,9 @@ function store_system(&$db_conn, $name_short, $filename)
 	$output = array();
 	$output[] = "name_short = \"".$name_short."\"";
 	$output[] = "name_manufacturer = \"".$res[0]['name_manufacturer']."\"";
-	$output[] = "target_file = \"".get_path("data_folder")."/enrichment/".$res[0]['target_file']."\"";
+	$roi = trim($res[0]['target_file']);
+	if ($roi!="") $roi = get_path("data_folder")."/enrichment/".$roi;
+	$output[] = "target_file = \"".$roi."\"";
 	$output[] = "adapter1_p5 = \"".$res[0]['adapter1_p5']."\"";
 	$output[] = "adapter2_p7 = \"".$res[0]['adapter2_p7']."\"";
 	$output[] = "shotgun = ".$res[0]['shotgun'];
@@ -1275,7 +1277,9 @@ function get_processed_sample_info(&$db_conn, $ps_name, $error_if_not_found=true
 	$info = $res[0];
 	
 	//prefix target region
-	$info['sys_target'] = get_path("data_folder")."/enrichment/".$info['sys_target'];
+	$roi = trim($info['sys_target']);
+	if ($roi!="") $roi = get_path("data_folder")."/enrichment/".$roi;
+	$info['sys_target'] = $roi;
 	
 	
 	//normal sample name (tumor reference sample)
