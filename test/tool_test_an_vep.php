684348756bd92f9e32decdc75fc1e9b85b0e1dc4
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2021-03-29 09:39:26 +0200

    Fixed wrong parameter in db_import_ancestry call

--- test/tool_test_an_vep.php
+++ test/tool_test_an_vep.php
@@ -58,6 +58,7 @@ function sort_consequences($filename)
 $out_file1 = output_folder().$name."_out1.vcf";
 check_exec("php ".src_folder()."/NGS/{$name}.php -test -in ".data_folder().$name."_in1.vcf -out $out_file1 --log ".output_folder().$name."_out1.log");
 remove_lines_containing($out_file1, array("##VEP=\"v"));
+remove_lines_containing($out_file1, array("##INFO=<ID=NGSD_GENE_INFO,"));
 sort_consequences($out_file1);
 check_file($out_file1, data_folder().$name."_out1.vcf", true);
 
@@ -65,31 +66,36 @@ check_file($out_file1, data_folder().$name."_out1.vcf", true);
 $out_file_empty = output_folder().$name."_out_empty.vcf";
 check_exec("php ".src_folder()."/NGS/{$name}.php -in ".data_folder().$name."_in_empty.vcf -out $out_file_empty --log ".output_folder().$name."_out_empty.log");
 remove_lines_containing($out_file_empty, array("##VEP=\"v"));
+remove_lines_containing($out_file_empty, array("##INFO=<ID=NGSD_GENE_INFO,"));
 check_file($out_file_empty, data_folder().$name."_out_empty.vcf", true);
 
 //NA12878_38 head
 $out_file2 = output_folder().$name."_out2.vcf";
 check_exec("php ".src_folder()."/NGS/{$name}.php -test -in ".data_folder().$name."_in2.vcf -out $out_file2 --log ".output_folder().$name."_out2.log");
 remove_lines_containing($out_file2, array("##VEP=\"v"));
+remove_lines_containing($out_file2, array("##INFO=<ID=NGSD_GENE_INFO,"));
 check_file($out_file2, data_folder().$name."_out2.vcf", true);
 
 //NA12878_38 head zipped
 $out_file3 = output_folder().$name."_out3.vcf";
 check_exec("php ".src_folder()."/NGS/{$name}.php -test -in ".data_folder().$name."_in3.vcf.gz -out $out_file3 --log ".output_folder().$name."_out3.log");
 remove_lines_containing($out_file3, array("##VEP=\"v"));
+remove_lines_containing($out_file3, array("##INFO=<ID=NGSD_GENE_INFO,"));
 check_file($out_file3, data_folder().$name."_out3.vcf", true);
 
 //zero variants to score with mmsplice
-$out_file1 = output_folder().$name."_out4.vcf";
-check_exec("php ".src_folder()."/NGS/{$name}.php -test -in ".data_folder().$name."_in4.vcf -out $out_file1 --log ".output_folder().$name."_out4.log");
-remove_lines_containing($out_file1, array("##VEP=\"v"));
-sort_consequences($out_file1);
-check_file($out_file1, data_folder().$name."_out4.vcf", true);
+$out_file4 = output_folder().$name."_out4.vcf";
+check_exec("php ".src_folder()."/NGS/{$name}.php -test -in ".data_folder().$name."_in4.vcf -out $out_file4 --log ".output_folder().$name."_out4.log");
+remove_lines_containing($out_file4, array("##VEP=\"v"));
+remove_lines_containing($out_file4, array("##INFO=<ID=NGSD_GENE_INFO,"));
+sort_consequences($out_file4);
+check_file($out_file4, data_folder().$name."_out4.vcf", true);
 
 //DRAGEN
 $out_file_dragen = output_folder().$name."_out_dragen.vcf";
 check_exec("php ".src_folder()."/NGS/{$name}.php -test -in ".data_folder().$name."_in_dragen.vcf -out $out_file_dragen --log ".output_folder().$name."_out_dragen.log");
 remove_lines_containing($out_file_dragen, array("##VEP=\"v"));
+remove_lines_containing($out_file_dragen, array("##INFO=<ID=NGSD_GENE_INFO,"));
 check_file($out_file_dragen, data_folder().$name."_out_dragen.vcf", true);
 
 //tests with NGSD disease group annotations - these tests will be run only if NGSD is available (i.e. not in nightly tests)
@@ -99,6 +105,7 @@ if (db_is_enabled("NGSD"))
 	$out_file_db1 = output_folder().$name."_out_db1.vcf";
 	check_exec("php ".src_folder()."/NGS/{$name}.php -test -ps_name NA12878_03 -in ".data_folder().$name."_in1.vcf -out $out_file_db1 --log ".output_folder().$name."_out_db1.log");
 	remove_lines_containing($out_file_db1, array("##VEP=\"v"));
+	remove_lines_containing($out_file_db1, array("##INFO=<ID=NGSD_GENE_INFO,"));
 	sort_consequences($out_file_db1);
 	check_file($out_file_db1, data_folder().$name."_out_db1.vcf", true);
 
@@ -106,12 +113,14 @@ if (db_is_enabled("NGSD"))
 	$out_file_db2 = output_folder().$name."_out_db2.vcf";
 	check_exec("php ".src_folder()."/NGS/{$name}.php -test -ps_name NA12878_38 -in ".data_folder().$name."_in2.vcf -out $out_file_db2 --log ".output_folder().$name."_out_db2.log");
 	remove_lines_containing($out_file_db2, array("##VEP=\"v"));
+	remove_lines_containing($out_file_db2, array("##INFO=<ID=NGSD_GENE_INFO,"));
 	check_file($out_file_db2, data_folder().$name."_out_db2.vcf", true);
 		
 	//somatic
 	$out_file_som = output_folder().$name."_out_som.vcf";
 	check_exec("php ".src_folder()."/NGS/{$name}.php -test -somatic -in ".data_folder().$name."_in1.vcf -out $out_file_som --log ".output_folder().$name."_out_som.log");
 	remove_lines_containing($out_file_som, array("##VEP=\"v"));
+	remove_lines_containing($out_file_som, array("##INFO=<ID=NGSD_GENE_INFO,"));
 	sort_consequences($out_file_som);
 	check_file($out_file_som, data_folder().$name."_out_som.vcf", true);
 }
@@ -120,6 +129,7 @@ if (db_is_enabled("NGSD"))
 $out_file_som_vicc = output_folder().$name."_out_som_vicc.vcf";
 check_exec("php ".src_folder()."/NGS/{$name}.php -test -in ".data_folder().$name."_in5_som_vicc.vcf -out $out_file_som_vicc -somatic -no_splice --log ".output_folder().$name."_out_som_vicc.log");
 remove_lines_containing($out_file_som_vicc, array("##VEP=\"v", "##INFO=<ID=NGSD_GROUP"));
+remove_lines_containing($out_file_som_vicc, array("##INFO=<ID=NGSD_GENE_INFO,"));
 check_file($out_file_som_vicc, data_folder().$name."_out_som_vicc.vcf", true);
 
 end_test();
