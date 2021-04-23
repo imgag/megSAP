79598d3986dc8cbb32f29d2ef58b73e3b7e41505
Marc Sturm <marc-sturm@users.noreply.github.com>
2021-04-23 08:37:10 +0200

    Fixed minor error in download_tools_vep.sh

--- data/download_tools_vep.sh
+++ data/download_tools_vep.sh
@@ -8,9 +8,9 @@ tools=$root/tools/
 dbs=$root/dbs/
 
 #Ignore this - used for local installation
-tools=/mnt/share/opt/
+# tools=/mnt/share/opt/
 # tools=/home/bioinf/
-dbs=/mnt/share/data/dbs/
+# dbs=/mnt/share/data/dbs/
 
 vep_install_dir=$tools/ensembl-vep-release-103.1/
 vep_cpan_dir=$vep_install_dir/cpan/
