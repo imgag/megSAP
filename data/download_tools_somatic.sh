512108e9a6e767688d37ad0af6368dfcc1d6f157
Leon Schütz <leonschuetz@users.noreply.github.com>
2021-06-11 11:36:54 +0200

    umiVar 2 (#91)
    * initial commit for umiVar2
    
    * src/Pipelines/analyze_cfdna.php: added support for non-personalized cfDNA
    
    * src/Pipelines/analyze_cfdna.php: Bufix: Fixed bug in reordering of command blocks
    
    doc/install_unix.md: updated install instruction
    
    data/install_deps_umiVar2.R: added required packages for umiVar2
    
    * data/download_tools_somatic.sh: added custom R installation for umiVar2
    
    data/install_deps_umiVar2.R: updated dependencies
    
    doc/install_unix.md: updated docu
    
    src/NGS/vc_cfdna.php: use custom R installation
    
    src/Pipelines/analyze_cfdna.php: check analysis type
    
    test/data_cfdna/*: updated test data
    
    * .gitignore: updated excludes for cfDNA
    
    * src/Pipelines/analyze_cfdna.php: added panel extraction from NGSD
    
    * src/Tools/export_cfdna_panel.php: added tool to export cfDNA panel
    
    * src/NGS/vc_cfdna.php: updated to new umiVar version
    
    src/Pipelines/analyze_cfdna.php: added monitoring bed and adapted to non-personalized samples
    
    test/data_cfdna/*: added additional test case and refactored pipeline test
    
    test/tool_test_vc_cfdna.php: added tool test
    
    * settings*: added umiVar2 path
    
    test/data_cfdna/CFDNA_0*: updated test files
    
    test/data/vc_cfdna_out1*: updated test files
    
    * test/tool_test_vc_cfdna.php: fixed bug with VCF fileDate

--- data/download_tools_somatic.sh
+++ data/download_tools_somatic.sh
@@ -2,13 +2,13 @@ set -e
 set -o pipefail
 set -o verbose
 
-folder=`pwd`/tools/
+root=`pwd`
+folder=$root/tools/
 cd $folder
 
 #download varscan2
 mkdir VarScan.v2.4.4
 wget https://github.com/dkoboldt/varscan/raw/master/VarScan.v2.4.4.jar -O VarScan.v2.4.4/VarScan.v2.4.4.jar
-
 
 #download strelka2
 wget https://github.com/Illumina/strelka/releases/download/v2.9.10/strelka-2.9.10.centos6_x86_64.tar.bz2
@@ -20,3 +20,27 @@ cd $folder
 wget https://github.com/OSU-SRLab/MANTIS/archive/v1.0.5.tar.gz
 tar xzf v1.0.5.tar.gz
 rm v1.0.5.tar.gz
+
+#download umiVar2
+cd $folder
+git clone https://github.com/imgag/umiVar2.git umiVar2
+cd umiVar2
+# create virtual environment
+python3 -m venv venv 
+source venv/bin/activate
+pip3 install pysam==0.16.0.1
+pip3 install numpy==1.19.5
+pip3 install scipy==1.5.4
+pip3 install networkx==2.5
+deactivate
+# create custom R installation
+wget https://cran.r-project.org/src/base/R-4/R-4.0.5.tar.gz
+tar -xvzf R-4.0.5.tar.gz
+rm R-4.0.5.tar.gz
+cd R-4.0.5
+./configure --with-pcre1
+make
+make check
+# install required packages
+bin/R -f $root/install_deps_umiVar2.R
+cd ../..
