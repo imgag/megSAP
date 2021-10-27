720434b47d3e6172298918463824d66ebbf317ea
Jakob Admard <jakob.admard@med.uni-tuebingen.de>
2021-10-27 15:09:47 +0200

    analyze_rna.php: add step for expression plots

--- src/Pipelines/analyze_rna.php
+++ src/Pipelines/analyze_rna.php
@@ -17,8 +17,8 @@ $parser->addString("name", "Base file name, typically the processed sample ID (e
 
 //optional
 $parser->addInfile("system", "Processing system INI file (determined from NGSD via the 'name' by default).", true);
-$steps_all = array("ma", "rc", "an", "fu", "db");
-$parser->addString("steps", "Comma-separated list of steps to perform:\nma=mapping, rc=read counting, an=annotation, fu=fusion detection, db=import into NGSD", true, "ma,rc,an,fu,db");
+$steps_all = array("ma", "rc", "an", "fu", "db", "plt");
+$parser->addString("steps", "Comma-separated list of steps to perform:\nma=mapping, rc=read counting, an=annotation, fu=fusion detection, db=import into NGSD", true, "ma,rc,an,fu,db,plt");
 
 $parser->addEnum("library_type", "Specify the library type, i.e. the strand R1 originates from (dUTP libraries correspond to reverse).", true, array("unstranded", "reverse", "forward"), "reverse");
 $parser->addFlag("no_splicing", "Disable spliced read alignment.");
@@ -324,6 +324,27 @@ SQL;
 		{
 			$parser->exec("{$ngsbits}SplicingToBed", "-in {$junctions} -report {$splicing_annot} -gene_report {$splicing_gene} -bed {$splicing_bed}", true);
 		}
+	}
+}
+
+//save gene expression plots
+if (in_array("plt", $steps))
+{
+	$args = [
+		"--cohort", $expr_cohort,
+		"--annotation", $expr,
+		"--sample", $name
+	];
+	$genelists = glob(get_path("data_folder") . "/pathway_genelists/*.txt");
+	foreach ($genelists as $genelist)
+	{
+		$shortname = basename($genelist, ".txt");
+		$args_extra = [
+			"--genelist", $genelist,
+			"--title", "\"Gene Expression: {$shortname}\"",
+			"--plot", "{$prefix}_expr.{$shortname}.png"
+		];
+		$parser->exec("python3 ".repository_basedir()."/src/NGS/rc_plot_expr.py", implode(" ", array_merge($args, $args_extra)), true);
 	}
 }
 
