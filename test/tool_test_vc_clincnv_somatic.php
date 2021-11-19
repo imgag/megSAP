c67fe793ec48419ef3bb0e72e18ee4a9f976aa50
Axel Gschwind <axel.gschwind@t-online.de>
2021-11-19 15:06:16 +0100

    tool_test_vc_clincnv_somatic.php: test case liftover to grch38

--- test/tool_test_vc_clincnv_somatic.php
+++ test/tool_test_vc_clincnv_somatic.php
@@ -30,17 +30,17 @@ $system_file = $cov_folder . "processing_system.ini";
 $bed_file = $cov_folder ."target_region.bed";
 
 
-$t_cov = "{$cov_folder_t}/DX000015_01.cov";
-$n_cov = "{$cov_folder_n}/DX000015_02.cov";
+$t_cov = "{$cov_folder_t}/DX000019_01.cov";
+$n_cov = "{$cov_folder_n}/DX000019_02.cov";
 
 $bed_file_off = $cov_folder . "off_target.bed";
 $out_file = output_folder().$name."_out.tsv";
 $cov_folder_n_off = $cov_folder ."cov-normal_off_target/";
 $cov_folder_t_off = $cov_folder ."cov-tumor_off_target/";
-$t_cov_off = $cov_folder_t_off . "DX000015_01.cov";
-$n_cov_off = $cov_folder_n_off . "DX000015_02.cov";
+$t_cov_off = $cov_folder_t_off . "DX000019_01.cov";
+$n_cov_off = $cov_folder_n_off . "DX000019_02.cov";
 
-check_exec("php ".src_folder()."/NGS/{$name}.php -t_id DX000015_01 -n_id DX000015_02 -cov_folder_n $cov_folder_n -cov_folder_t $cov_folder_t -cohort_folder $cohort_folder -cov_pairs $t_n_pair_file -out $out_file -t_cov $t_cov -n_cov $n_cov -bed $bed_file -system $system_file -t_cov_off $t_cov_off -n_cov_off $n_cov_off -cov_folder_n_off $cov_folder_n_off -cov_folder_t_off $cov_folder_t_off -bed_off $bed_file_off -baf_folder $baf_folder");
+check_exec("php ".src_folder()."/NGS/{$name}.php -t_id DX000019_01 -n_id DX000019_02 -cov_folder_n $cov_folder_n -cov_folder_t $cov_folder_t -cohort_folder $cohort_folder -cov_pairs $t_n_pair_file -out $out_file -t_cov $t_cov -n_cov $n_cov -bed $bed_file -system $system_file -t_cov_off $t_cov_off -n_cov_off $n_cov_off -cov_folder_n_off $cov_folder_n_off -cov_folder_t_off $cov_folder_t_off -bed_off $bed_file_off -baf_folder $baf_folder");
 
 // results depend on the availability of the NGSD 
 if (db_is_enabled("NGSD"))
@@ -52,9 +52,9 @@ else
 	check_file($out_file, data_folder().$name."_out_noNGSD.tsv");
 }
 
-check_file_exists(output_folder()."/DX000015_01-DX000015_02_clonality.png");
-check_file_exists(output_folder()."/DX000015_01-DX000015_02_clonalityBarplot.png");
-check_file_exists(output_folder()."/DX000015_01-DX000015_02_CNAs_plot.png");
+check_file_exists(output_folder()."/DX000019_01-DX000019_02_clonality.png");
+check_file_exists(output_folder()."/DX000019_01-DX000019_02_clonalityBarplot.png");
+check_file_exists(output_folder()."/DX000019_01-DX000019_02_CNAs_plot.png");
 
 end_test();
 
