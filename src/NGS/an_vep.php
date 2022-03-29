65356e6a413981baf5ad9c613dd562b0b4b6aee2
Alexander Ott <Alexander.Ott@med.uni-tuebingen.de>
2022-03-29 09:14:04 +0200

    add debug function

--- src/NGS/an_vep.php
+++ src/NGS/an_vep.php
@@ -51,6 +51,11 @@ function annotation_file_path($rel_path, $is_optional=false)
 		return $orig;
 	}
 	
+	if (mb_strpos($copy, "gnomAD_genome_v3.1.1_GRCh38.vcf") !== false)
+	{
+		// TODO: REMOVE this if block!
+		return "/mnt/users/ahott1a1/megSap2/data/dbs/gnomAD/gnomAD_genome_v3.1.1_GRCh38.vcf.gz";
+	}
 	return $copy;
 }
 
