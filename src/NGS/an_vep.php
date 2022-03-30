150238ad33236f0fb9891c493fc329538d093d1e
Alexander Ott <Alexander.Ott@med.uni-tuebingen.de>
2022-03-30 09:09:11 +0200

    an_vep: update tests

--- src/NGS/an_vep.php
+++ src/NGS/an_vep.php
@@ -53,9 +53,10 @@ function annotation_file_path($rel_path, $is_optional=false)
 	
 	if (mb_strpos($copy, "gnomAD_genome_v3.1.1_GRCh38.vcf") !== false)
 	{
-		// TODO: REMOVE this if block!
+		// @todo: REMOVE this if block!
 		return "/mnt/users/ahott1a1/megSap2/data/dbs/gnomAD/gnomAD_genome_v3.1.1_GRCh38.vcf.gz";
 	}
+	
 	return $copy;
 }
 
@@ -473,7 +474,7 @@ $config_file = fopen2($config_file_path, 'w');
 
 
 // add gnomAD annotation
-fwrite($config_file, annotation_file_path("/dbs/gnomAD/gnomAD_genome_v3.1.1_GRCh38.vcf.gz")."\tgnomADg\tAF,Hom,Hemi,Het,Wt,AC\t\ttrue\n");
+fwrite($config_file, annotation_file_path("/dbs/gnomAD/gnomAD_genome_v3.1.1_GRCh38.vcf.gz")."\tgnomADg\tAC,AF,Hom,Hemi,Het,Wt\t\ttrue\n");
 fwrite($config_file, annotation_file_path("/dbs/gnomAD/gnomAD_genome_v3.1.mito_GRCh38.vcf.gz")."\tgnomADm\tAF_hom\t\ttrue\n");
 
 // add clinVar annotation
