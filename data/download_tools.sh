8a34123e3f2dad4e9717241ab8594207310389ac
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2022-10-05 14:01:51 +0200

    Updated ngs-bits version.

--- data/download_tools.sh
+++ data/download_tools.sh
@@ -7,7 +7,7 @@ folder=$root/tools/
 cd $folder
 git clone https://github.com/imgag/ngs-bits.git
 cd ngs-bits
-git checkout 2022_07 && git submodule update --recursive --init
+git checkout 2022_10 && git submodule update --recursive --init
 make build_3rdparty
 make build_tools_release
 
