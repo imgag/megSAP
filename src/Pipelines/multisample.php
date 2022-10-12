5d247ad4577ca3e2d8fad2881ffa4e5030efe5d7
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2022-10-12 10:42:01 +0200

    Updated medical databases

--- src/Pipelines/multisample.php
+++ src/Pipelines/multisample.php
@@ -42,7 +42,7 @@ extract($parser->parse($argv));
 $repository_basedir = repository_basedir();
 $data_folder = get_path("data_folder");
 $ngsbits = get_path("ngs-bits");
-$hgmd_file = "{$data_folder}/dbs/HGMD/HGMD_CNVS_2022_2.bed";
+$hgmd_file = "{$data_folder}/dbs/HGMD/HGMD_CNVS_2022_3.bed";
 $omim_file = "{$data_folder}/dbs/OMIM/omim.bed";
 $vcf_all = "{$out_folder}/all.vcf.gz";
 $vcf_all_mito = "{$out_folder}/all_mito.vcf.gz";
@@ -756,7 +756,7 @@ if (in_array("cn", $steps))
 		$parser->exec("{$ngsbits}BedAnnotateFromBed", "-in {$cnv_multi} -in2 {$data_folder}/dbs/ClinGen/dosage_sensitive_disease_genes_GRCh38.bed -no_duplicates -out {$cnv_multi}", true);
 		
 		//pathogenic ClinVar CNVs
-		$parser->exec("{$ngsbits}BedAnnotateFromBed", "-in {$cnv_multi} -in2 {$data_folder}/dbs/ClinVar/clinvar_cnvs_2022-07.bed -name clinvar_cnvs -url_decode -no_duplicates -out {$cnv_multi}", true);
+		$parser->exec("{$ngsbits}BedAnnotateFromBed", "-in {$cnv_multi} -in2 {$data_folder}/dbs/ClinVar/clinvar_cnvs_2022-10.bed -name clinvar_cnvs -url_decode -no_duplicates -out {$cnv_multi}", true);
 		
 		//HGMD CNVs
 		if (file_exists($hgmd_file)) //optional because of license
