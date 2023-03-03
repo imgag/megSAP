09cf8e7323e433de7c9ef00d9c731a3388eff0a4
Ott-Alexander <Alexander.Ott@med.uni-tuebingen.de>
2023-03-03 11:22:43 +0100

    extractSignatures: allow empty input files

--- test/tool_test_extract_signatures.php
+++ test/tool_test_extract_signatures.php
@@ -18,6 +18,9 @@ check_file($out_file_ID, data_folder().$name."_out0_ID83.csv", true);
 $out_file_DBS = $out."De_Novo_map_to_COSMIC_DBS78.csv";
 check_file($out_file_DBS, data_folder().$name."_out0_DBS78.csv", true);
 
+//snvs - empty file
+$out = output_folder()."/{$name}_out3/";
+check_exec(get_path("python3")." ".src_folder()."/NGS/{$name}.py --in ".data_folder()."/extract_signatures_in3.vcf --mode snv --reference GRCh38 --threads 4 --outFolder $out --seeds $seeds --nmfReplicates 10");
 
 //cnvs
 $out = output_folder()."/{$name}_out1/";
@@ -26,6 +29,9 @@ check_exec(get_path("python3")." ".src_folder()."/NGS/{$name}.py --in ".data_fol
 $out_file_CNV = $out."De_Novo_map_to_COSMIC_CNV48.csv";
 check_file($out_file_CNV, data_folder().$name."_clincnv_out1_CNV48.csv", true);
 
+//cnvs empty file (no cnvs):
+$out = output_folder()."/{$name}_out2/";
+check_exec(get_path("python3")." ".src_folder()."/NGS/{$name}.py --in ".data_folder()."/extract_signatures_clincnv_in2.tsv --mode cnv --reference GRCh38 --threads 4 --outFolder $out --seeds $seeds --nmfReplicates 10");
 
 end_test();
 
