80d1039fe4ea6d0e69f08774e370220312af4d38
Leon Schütz <leon.schuetz@med.uni-tuebingen.de>
2023-09-27 11:23:31 +0200

    src/NGS/mapping_dragen.php: added gVCF output

--- src/Pipelines/mapping.php
+++ src/Pipelines/mapping.php
@@ -293,6 +293,7 @@ if ($use_dragen)
 {
 	$dragen_output_bam = "$dragen_output_folder/{$out_name}_dragen.bam";
 	$dragen_output_vcf = "$dragen_output_folder/{$out_name}_dragen.vcf.gz";
+	$dragen_output_gvcf = "$dragen_output_folder/{$out_name}_dragen.gvcf.gz";
 	$dragen_output_sv = "$dragen_output_folder/{$out_name}_dragen_svs.vcf.gz";
 	$dragen_log_file = "$dragen_output_folder/{$out_name}_dragen.log";
 	$sge_update_interval = 300; //5min
@@ -303,6 +304,7 @@ if ($use_dragen)
 	$args[] = "-in2 ".$trimmed2_dragen;
 	$args[] = "-out ".$dragen_output_bam;
 	$args[] = "-out_vcf ".$dragen_output_vcf;
+	$args[] = "-out_gvcf ".$dragen_output_gvcf;
 	$args[] = "-out_sv ".$dragen_output_sv;
 	$args[] = "-sample ".$out_name;
 	$args[] = "-build ".$build;
@@ -403,6 +405,8 @@ if ($use_dragen)
 	}
 	$parser->moveFile($dragen_output_vcf, $dragen_call_folder.basename($dragen_output_vcf));
 	$parser->moveFile($dragen_output_vcf.".tbi", $dragen_call_folder.basename($dragen_output_vcf).".tbi");
+	$parser->moveFile($dragen_output_gvcf, $dragen_call_folder.basename($dragen_output_gvcf));
+	$parser->moveFile($dragen_output_gvcf.".tbi", $dragen_call_folder.basename($dragen_output_gvcf).".tbi");
 	if (get_path("dragen_sv_calling"))
 	{
 		$parser->moveFile($dragen_output_sv, $dragen_call_folder.basename($dragen_output_sv));
