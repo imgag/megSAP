11ac4363a998480d3229af9b42bec29b7e0c02eb
Leon Schütz <leon.schuetz@med.uni-tuebingen.de>
2024-05-08 11:55:56 +0200

    src/NGS/vc_clair.php: added todo
    src/NGS/vcf_fix.php: use max AF on multiallelic varaints
    
    test/tool_test_mapping_minimap.php: check test results based on bam file
    
    test/data_longread/Makefile: remove also crm files

--- src/NGS/vc_clair.php
+++ src/NGS/vc_clair.php
@@ -96,6 +96,7 @@ $pipeline[] = array(get_path("ngs-bits")."VcfFilter", "-qual 5 -remove_invalid")
 //this step has to be performed before vcfbreakmulti - otherwise mulitallelic variants that contain both 'hom' and 'het' genotypes fail - see NA12878 amplicon test chr2:215632236-215632276
 $pipeline[] = array(get_path("vcflib")."vcfallelicprimitives", "-kg");
 
+//TODO: check ngs-bits VcfBreakMulti to fix problems with Format AF column
 //split multi-allelic variants
 $pipeline[] = array(get_path("vcflib")."vcfbreakmulti", "");
 
--- test/tool_test_mapping_minimap.php
+++ test/tool_test_mapping_minimap.php
@@ -22,8 +22,10 @@ check_file($out_file2, data_folder().$name."_out1.bam");
 
 //cram output
 $out_file3 = output_folder().$name."_out3.cram";
+$out_file3_bam = output_folder().$name."_out3.bam";
 check_exec("php ".src_folder()."/NGS/{$name}.php -in_bam {$in2_file} -system {$in1_system} -out {$out_file3} --log ".output_folder().$name."_out2.log -threads 1");
-check_file($out_file2, data_folder().$name."_out3.cram");
+check_exec(get_path("samtools")." view -h -b {$out_file3} > {$out_file3_bam}");
+check_file($out_file3_bam, data_folder().$name."_out3.bam");
 
 
 end_test();
