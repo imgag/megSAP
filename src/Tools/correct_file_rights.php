b7d09695c4427d5d2fe2d7bcb2661b27cf02798c
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2024-07-11 15:21:28 +0200

    correct_file_rights: changed input from project type list to folder list

--- src/Tools/correct_file_rights.php
+++ src/Tools/correct_file_rights.php
@@ -9,28 +9,15 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 //parse command line arguments
 $parser = new ToolBase("correct_file_rights", "Corrects file owner and permissions.");
-$parser->addString("types", "Comma-separated list of project types.", false);
+$parser->addStringArray("folders", "Folders to change.", false);
 $parser->addString("owner", "File owner.", true, "bioinf");
 $parser->addString("group", "File group.", true, "f_ad_bi_l_medgen_access_storages");
 $parser->addString("permissions", "File permissions.", true, "775");
 extract($parser->parse($argv));
 
-//init
-$db = DB::getInstance("NGSD");
-$project_folders  = get_path("project_folder");
-
-//check types
-$project_types = explode(",", $types);
-$project_types = array_map('trim', $project_types);
-$valid_types = $db->getEnum("project", "type");
-foreach($project_types as $type)
+foreach($folders as $folder)
 {
-	if (!in_array($type, $valid_types)) trigger_error("Invalid type '$type'!", E_USER_ERROR);
-}
-
-foreach($project_types as $type)
-{
-	$folder = $project_folders[$type];
+	$folder = trim($folder);
 	
 	//change permissions (before user and group to make sure the files that are currently being written are still writable)
 	$time_start = microtime(true);
