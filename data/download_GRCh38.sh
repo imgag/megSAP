1098db5cb2d7371cd734f4cff02e38b33c37fa85
marc-sturm <marc.sturm@med.uni-tuebingen.de>
2024-09-17 14:53:28 +0200

    replaced bedtools genome masking tool by ngs-bits

--- data/download_GRCh38.sh
+++ data/download_GRCh38.sh
@@ -19,7 +19,7 @@ if [ -z "$1" ]
 fi
 rm -rf $genome $genome.fai
 
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz
 (gunzip -c GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz |  sed -r 's/>chrM/>chrMT/g' > $genome) || true
 rm GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz
 
