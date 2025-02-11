32653aecc1a40b65baf387e4bbf36f138ebbad83
Kilian Ilius <kilian.ilius@med.uni-tuebingen.de>
2025-02-11 13:13:40 +0100

    Adapted build_ngsbits_container.php script so any apptainer container for megSAP can be build.

--- src/IMGAG/build_apptainer_container.php
+++ src/IMGAG/build_apptainer_container.php
@@ -1,6 +1,6 @@
 <?php
 /** 
-	@page build_ngsbits_container
+	@page build_apptainer_container
 */
 
 require_once(dirname($_SERVER['SCRIPT_FILENAME'])."/../Common/all.php");
@@ -8,7 +8,8 @@ require_once(dirname($_SERVER['SCRIPT_FILENAME'])."/../Common/all.php");
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 //parse command line arguments
-$parser = new ToolBase("build_ngsbits_container", "Builds a ngs-bits container.");
+$parser = new ToolBase("build_apptainer_container", "Builds a apptainer container for a specified tool.");
+$parser->addString("tool", "Tool to build a container for", false);
 $parser->addString("tag", "Tag to build.", true, "master");
 extract($parser->parse($argv));
 
@@ -20,24 +21,29 @@ if (contains($tag, "_"))
 }
 
 //build
-$sif = "ngs-bits_{$tag}.sif";
-$log = "ngs-bits_{$tag}.log";
+$sif = "{$tool}_{$tag}.sif";
+$log = "{$tool}_{$tag}.log";
 print "Building container {$sif} - in case of error see {$log}\n";
-exec2("apptainer build {$sif} data/tools/container_recipes/ngs-bits_{$tag}.def > $log 2>&1");
+exec2("apptainer build {$sif} data/tools/container_recipes/{$tool}_{$tag}.def > $log 2>&1");
 exec2("chmod 777 {$sif}");
 
-//determine version
-list($stdout) = exec2("apptainer exec {$sif} MappingQC --version");
-$version = trim(strtr(implode("", $stdout), ["MappingQC"=>""]));
-print "Building container finished.\n";
-print "ngs-bits version determined from container: {$version}\n";
-
-//determine final container name
 $sif2 = $sif;
-if($tag=="master")
+
+if ($tool =="ngs-bits")
 {
-	$sif2 = "ngs-bits_master-".strtr($version, "_", "-").".sif";
+	//determine version
+	list($stdout) = exec2("apptainer exec {$sif} MappingQC --version");
+	$version = trim(strtr(implode("", $stdout), ["MappingQC"=>""]));
+	print "Building container finished.\n";
+	print "ngs-bits version determined from container: {$version}\n";
+
+	//determine final container name
+	if($tag=="master")
+	{
+		$sif2 = "ngs-bits_master-".strtr($version, "_", "-").".sif";
+	}
 }
+
 
 //move to container repository
 $container_repo = "/mnt/storage2/megSAP/tools/apptainer_container/";
