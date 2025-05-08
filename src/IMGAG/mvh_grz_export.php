65528ec2841b41b1ed770021e231cfd2537b592d
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2025-05-08 14:37:55 +0200

    update export script

--- src/IMGAG/mvh_grz_export.php
+++ src/IMGAG/mvh_grz_export.php
@@ -7,9 +7,37 @@ require_once(dirname($_SERVER['SCRIPT_FILENAME'])."/../Common/all.php");
 
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
+//convert NGSD gender to JSON schema
+function convert_gender($gender)
+{
+	if ($gender="n/a") $gender = "unknown";
+	return $gender;
+}
+
+//returns case management data as XML
+function get_cm_data($case_id)
+{
+	global $db_mvh;
+	
+	$lines = explode("\n", $db_mvh->getValue("SELECT cm_data FROM case_data WHERE id='{$case_id}'", ""));
+	$lines_not_repeat = [];
+	foreach($lines as $line)
+	{
+		$line = trim($line);
+		if ($line=="") continue;
+		if (strpos($line, "<redcap_repeat_instance></redcap_repeat_instance>")===false) continue;
+
+		$lines_not_repeat[] = $line;
+	}
+	if (count($lines_not_repeat)!=1) trigger_error("Could not parse CM data. ".count($lines_not_repeat)." XML items which are not 'redcap_repeat_instance'!", E_USER_ERROR);
+	
+	return simplexml_load_string($lines_not_repeat[0], "SimpleXMLElement", LIBXML_NOCDATA);;
+}
+
+
 //parse command line arguments
 $parser = new ToolBase("mvh_grz_export", "GRZ export for Modellvorhaben.");
-$parser->addInt("case_id", "Case ID in Modellvorhaben DB.", false);
+$parser->addInt("case_id", "'id' in 'data_data' of 'MVH' database.", false);
 extract($parser->parse($argv));
 
 //init
@@ -18,48 +46,73 @@ $db_ngsd = DB::getInstance("NGSD");
 $mvh_folder = get_path("mvh_folder");
 
 //check that case ID is valid
-$seq_type = $db_mvh->getValue("SELECT seq_type FROM case_data WHERE id='{$case_id}'", "");
-if ($seq_type=="") trigger_error("No case with id '{$case}' in MVH database!", E_USER_ERROR);
-if ($seq_type!="wes") trigger_error("Type '{$seq_type}' not supported yet!", E_USER_ERROR); //TODO implement WGS, lrGS, TN WES, TN WGS
-$disease_type = $db_mvh->getValue("SELECT disease_type FROM case_data WHERE id='{$case_id}'");
-print "case: {$case_id} (seq_type: {$seq_type}, disease_type: {$disease_type})\n";
+$id = $db_mvh->getValue("SELECT id FROM case_data WHERE id='{$case_id}'");
+if ($id=="") trigger_error("No case with id '{$case}' in MVH database!", E_USER_ERROR);
 
-//determine KDK
+//parse case management data
+$cm_data = get_cm_data($case_id);
+
+//TODO add other networks
+//check network > determine KDK and study_subtype
+$network = $cm_data->network_title;
 $kdk = "";
-if ($disease_type=="rare") $kdk = "KDKTUE002"; //NSE - Tübingen
-if ($disease_type=="oncological") $kdk = "KDKTUE005"; //DNPM - Tübingen
-if ($disease_type=="hboc") $kdk = "KDKL00003"; //DK-FBREK - Leipzig
-if ($disease_type=="hereditary_cancer") $kdk = "KDKL00004"; //DK-FDK - Leipzig
-if ($kdk=="") trigger_error("Could not determine KDK for sample '{$ps1}' with disease_type '{$disease_type}'!", E_USER_ERROR);
-	
+$study_subtype = "";
+$$disease_type = "";
+if ($network="Netzwerk Seltene Erkrankungen")
+{
+	$kdk = "KDKTUE002"; //NSE - Tübingen
+	$study_subtype = "germline-only";
+	$disease_type = "rare";
+}
+else if ($network=="Deutsches Netzwerk für Personalisierte Medizin")
+{
+	$kdk = "KDKTUE005"; //DNPM - Tübingen
+	$study_subtype = "tumor+germline";
+	$disease_type = "oncological";
+}
+else if ($network=="Deutsches Konsortium Familiärer Brust- und Eierstockkrebs")
+{
+	$kdk = "KDKL00003"; //DK-FBREK - Leipzig
+	$study_subtype = "tumor+germline";
+	$disease_type = "oncological";
+}
+else trigger_error("Unhandled network type '{$network}'!", E_USER_ERROR); 
+
+//check seqencing mode
+$seq_mode = $cm_data->seq_mode;
+if ($seq_mode!="WGS" && $seq_mode!="WES") trigger_error("Unhandled seq_mode '{$seq_mode}'!", E_USER_ERROR);
+
+print "case: {$case_id} (seq_mode: {$seq_mode} / network: {$network})\n";
+
 //check germline processed sample is ok
-$ps1 = $db_mvh->getValue("SELECT ps1 FROM case_data WHERE id='{$case_id}'");
-$info1 = get_processed_sample_info($db_ngsd, $ps1);
-$sys1 = $info1['sys_name_short'];
-$patient_id = $info1['patient_identifier'];
-if ($patient_id=="") trigger_error("No patient identifier set for sample '{$ps1}''!", E_USER_ERROR);
-print "sample 1: {$ps1} (system: {$sys1})\n";
-$readl1 = get_processed_sample_qc($db_ngsd, $ps1, "QC:2000006");
-while (contains($readl1, "-")) $readl1 = explode("-", $readl1, 2)[1];
+$ps = $db_mvh->getValue("SELECT ps FROM case_data WHERE id='{$case_id}'");
+$info = get_processed_sample_info($db_ngsd, $ps);
+$sys = $info['sys_name_short'];
+$patient_id = $info['patient_identifier'];
+if ($patient_id=="") trigger_error("No patient identifier set for sample '{$ps}''!", E_USER_ERROR);
+print "germline sample: {$ps} (system: {$sys})\n";
+
+//check germline processed sample is ok
+$ps_t = "";
+if ($study_subtype=="tumor+germline")
+{
+	$ps_t = $db_mvh->getValue("SELECT ps_t FROM case_data WHERE id='{$case_id}'");
+	if ($ps_t=="") trigger_error("Could no tumor sample for case '{$case_id}' in network '{$network}'!", E_USER_ERROR);
+	
+	//check tumor and germline have same system
+	$info_t = get_processed_sample_info($db_ngsd, $ps_t);
+	$sys_t = $info_t['sys_name_short'];
+	if ($sys!=$sys_t) trigger_error("Mismatching tumor/normal processing systems for case '{$case_id}' in network '{$network}': '{$sys}' vs '{$sys_t}'", E_USER_ERROR);
+}
 
 //determine ROI
 $roi = "";
-if ($sys1=="twistCustomExomeV2" || $sys1=="twistCustomExomeV2Covaris") $roi = "{$mvh_folder}/rois/twist_exome_core_plus_refseq.bed";
-if ($sys1=="hpHBOCv5") $roi = "/mnt/storage2/megSAP/data/enrichment/hpHBOCv5_2014_10_27.bed"; //TODO: only for testing > remove!
-if (!contains($seq_type, "wgs") && !contains($seq_type,"lrgs") && $roi=="") trigger_error("Could not determine target region for sample '{$ps1}'' with processing system '{$sys1}'!", E_USER_ERROR);
-
-//determine study sub-type
-$study_subtype = "";
-if ($seq_type=="wes" || $seq_type=="wgs" || $seq_type=="lrgs") $study_subtype = "germline-only";
-if ($seq_type=="wes_tn" || $seq_type=="wgs_tn") $study_subtype = "tumor+germline";
-if ($study_subtype=="") trigger_error("Could not determine strudy subtype for case {$case_id}!", E_USER_ERROR);
-
-//check tumor sample
-//TODO exists
-//TODO system same as germline sample
+if ($sys=="twistCustomExomeV2" || $sys=="twistCustomExomeV2Covaris") $roi = "{$mvh_folder}/rois/twist_exome_core_plus_refseq.bed";
+if ($sys=="hpHBOCv5") $roi = "/mnt/storage2/megSAP/data/enrichment/hpHBOCv5_2014_10_27.bed"; //TODO: only for testing > remove!
+if ($seq_mode!="WGS" && $roi=="") trigger_error("Could not determine target region for sample '{$ps}' with processing system '{$sys}'!", E_USER_ERROR);
 
 //determine tanG==VNg
-$sub_ids = $db_mvh->getValues("SELECT id FROM `submission_grz` WHERE status='pending'");
+$sub_ids = $db_mvh->getValues("SELECT id FROM `submission_grz` WHERE status='pending' AND case_id='$case_id}'");
 if (count($sub_ids)!=1)  trigger_error(count($sub_ids)." pending GRZ submissions for case {$case_id}. Must be one!", E_USER_ERROR);
 $sub_id = $sub_ids[0];
 $tang = $db_mvh->getValue("SELECT tang FROM submission_grz WHERE id='{$sub_id}'");
@@ -71,9 +124,12 @@ print "export folder: {$folder}\n";
 exec2("mkdir -p {$folder}/files/");
 exec2("mkdir -p {$folder}/metadata/");
 
+//TODO also handle tumor/normal
 //create raw data (FASTQs + target region)
-$bam1 = $info1['ps_bam'];
-$parser->execApptainer("ngs-bits", "BamToFastq", "-in {$bam1} -out1 {$folder}/files/{$tang}.read1.fastq.gz -out2 {$folder}/files/{$tang}.read2.fastq.gz -extend {$readl1}", [$bam1], ["{$folder}/files/"]);
+$bam = $info['ps_bam'];
+$read_length = get_processed_sample_qc($db_ngsd, $ps, "QC:2000006");
+while (contains($read_length, "-")) $read_length = explode("-", $read_length, 2)[1];
+$parser->execApptainer("ngs-bits", "BamToFastq", "-in {$bam} -out1 {$folder}/files/{$tang}.read1.fastq.gz -out2 {$folder}/files/{$tang}.read2.fastq.gz -extend {$read_length}", [$bam], ["{$folder}/files/"]);
 if ($roi!="") exec2("cp {$roi} {$folder}/files/target_regions.bed");
 
 //create meta data
@@ -83,19 +139,50 @@ $json[] = "\"\$schema\": \"https://raw.githubusercontent.com/BfArM-MVH/MVGenomse
 $json[] = "  \"submission\": {\n";
 $json[] = "    \"submissionDate\": \"".$db_mvh->getValue("SELECT date FROM submission_grz WHERE id='{$sub_id}'")."\",\n";
 $json[] = "    \"submissionType\": \"".$db_mvh->getValue("SELECT type FROM submission_grz WHERE id='{$sub_id}'")."\",\n";
-$json[] = "    \"submitterId\": \"260840108\",\n";
 $json[] = "    \"tanG\": \"{$tang}\",\n";
 $json[] = "    \"localCaseId\": \"{$patient_id}\",\n";
+$json[] = "    \"coverageType\": \"UNK\",\n"; //TODO get from GenLab
+$json[] = "    \"submitterId\": \"260840108\",\n";
 $json[] = "    \"genomicDataCenterId\": \"GRZTUE002\",\n";
 $json[] = "    \"clinicalDataNodeId\": \"{$kdk}\",\n";
-$json[] = "    \"labName\": \"Institute of Medical Genetics and Applied Genomics, Tuebingen, Germany\",\n";
+$json[] = "    \"diseaseType\": \"{$disease_type}\"\n";
 $json[] = "    \"genomicStudyType\": \"single\",\n";
 $json[] = "    \"genomicStudySubtype\": \"{$study_subtype}\",\n";
-$json[] = "    \"coverageType\": \"UNK\",\n"; //TODO get info and implement: "GKV" --> Gesetzliche Krankenversicherung "PKV" --> Private Krankenversicherung "BG", --> Berufsgenossenschaft "SEL", --> Selbstzahler "SOZ", --> Sozialamt "GPV", --> Gesetzliche Pflegeversicherung "PPV", --> Private Pflegeversicherung "BEI", --> Beihilfe "SKT", --> Sonstige Kostenträger "UNK" --> Unklar/Unbekannt
-$json[] = "    \"diseaseType\": \"{$disease_type}\"\n";
+$json[] = "    \"labName\": \"Institute of Medical Genetics and Applied Genomics, Tuebingen, Germany\",\n";
 $json[] = "  },\n";
 $json[] = "  \"donors\":\n";
 $json[] = "  [\n";
+$json[] = "    {\n";
+$json[] = "      \"donorPseudonym\": \"{$tang}\",\n";
+$json[] = "      \"gender\": \"".convert_gender($info["gender"])."\",\n";
+$json[] = "      \"relation\": \"index\",\n";
+$json[] = "      \"mvConsent\": {\n";
+$json[] = "        \"presentationDate\": \"".$cm_data->mvconsentpresenteddate."\",\n";
+$json[] = "        \"version\": \"".$cm_data->version_teilnahme."\",\n";
+$json[] = "        \"scope\":\n";
+$json[] = "         [\n";
+$json[] = "          {\n";
+$json[] = "            \"type\": \"".($cm_data->particip_4=="Ja" ? "permit" : "deny")."\",\n";
+$json[] = "            \"date\": \"".$cm_data->datum_teilnahme."\",\n";
+$json[] = "            \"domain\": \"mvSequencing\",\n";
+$json[] = "          },\n";
+$json[] = "          {\n";
+$json[] = "            \"type\": \"".($cm_data->particip_4_1=="Ja" ? "permit" : "deny")."\",\n";
+$json[] = "            \"date\": \"".$cm_data->datum_teilnahme."\",\n";
+$json[] = "            \"domain\": \"caseIdentification\",\n";
+$json[] = "          },\n";
+$json[] = "          {\n";
+$json[] = "            \"type\": \"".($cm_data->particip_4_2=="Ja" ? "permit" : "deny")."\",\n";
+$json[] = "            \"date\": \"".$cm_data->datum_teilnahme."\",\n";
+$json[] = "            \"domain\": \"reIdentification\",\n";
+$json[] = "          }\n";
+$json[] = "         ]\n";
+$json[] = "      },\n";
+//TODO researchConsents
+$json[] = "      \"labData\":\n"; //TODO
+$json[] = "        [\n";
+$json[] = "        ]\n";
+$json[] = "    }\n";
 $json[] = "  ]\n";
 $json[] = "{\n";
 print_r($json);
@@ -112,7 +199,7 @@ print_r($json);
 
 
 /*
-TODO:
+#TODO:
 - test if export works with other user, e.g. bioinf
 - store schema of MVH DB
 
@@ -130,4 +217,7 @@ TODO:
 	- Activate:
 		> /mnt/storage2/megSAP/tools/miniforge3/bin/conda activate grz-tools
 */
+
 ?>
+
+
