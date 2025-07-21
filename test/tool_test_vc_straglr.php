3e840e83f89416f5e6bc5703d989c616d448be41
Leon Schütz <leon.schuetz@med.uni-tuebingen.de>
2025-07-21 13:53:24 +0200

    settings_nightly.ini: updated straglr and straglrOn version
    src/Pipelines/analyze_longread.php: include partials for straglr RE calling on default
    
    src/Tools/vc_straglr.php: annotate wildtype copy number to VCF and fix allele counts
    
    test/data/vc_straglr*: updated test files

--- test/tool_test_vc_straglr.php
+++ test/tool_test_vc_straglr.php
@@ -21,7 +21,40 @@ foreach(array("FXN", "ATXN3", "DMPK") as $re)
     remove_lines_containing($out_file_svg_prefix.$re."_hist.svg", array("<dc:date>")); 
     check_file($out_file_svg_prefix.$re."_hist.svg", data_folder().$name."_out1_".$re."_hist.svg");
 }
+/*
+$out_file_vcf = output_folder().$name."_out1.vcf";
+$out_file_bed = output_folder().$name."_out1.bed";
+$out_file_log = output_folder().$name."_out1.log";
+$out_file_svg_prefix = output_folder()."repeat_expansions/{$name}_out1_";
+$variant_catalog = data_folder().$name."_variant_catalog_in1.bed";
+check_exec("php ".src_folder()."/Tools/".$name.".php -in ".data_folder().$name."_in1.bam -out $out_file_bed -loci {$variant_catalog} --log {$out_file_log} -include_partials -test");
+remove_lines_containing($out_file_vcf, array("##fileDate=", "##reference="));
+check_file($out_file_vcf, data_folder().$name."_out1.vcf");
+check_file($out_file_bed, data_folder().$name."_out1.bed");
+foreach(array("FXN", "ATXN3", "DMPK") as $re)
+{
+    check_file($out_file_svg_prefix.$re.".fa", data_folder().$name."_out1_".$re.".fa");
+    remove_lines_containing($out_file_svg_prefix.$re."_hist.svg", array("<dc:date>")); 
+    check_file($out_file_svg_prefix.$re."_hist.svg", data_folder().$name."_out1_".$re."_hist.svg");
+}
 
+
+$out_file2_vcf = output_folder().$name."_out2.vcf";
+$out_file2_bed = output_folder().$name."_out2.bed";
+$out_file2_log = output_folder().$name."_out2.log";
+$out_file2_svg_prefix = output_folder()."repeat_expansions/{$name}_out2_";
+$variant_catalog = data_folder().$name."_variant_catalog_in1.bed";
+check_exec("php ".src_folder()."/Tools/".$name.".php -include_partials -in ".data_folder().$name."_in1.bam -out $out_file2_bed -loci {$variant_catalog} --log {$out_file2_log} -test");
+remove_lines_containing($out_file2_vcf, array("##fileDate=", "##reference="));
+check_file($out_file2_vcf, data_folder().$name."_out2.vcf");
+check_file($out_file2_bed, data_folder().$name."_out2.bed");
+foreach(array("FXN", "ATXN3", "DMPK") as $re)
+{
+    check_file($out_file2_svg_prefix.$re.".fa", data_folder().$name."_out2_".$re.".fa");
+    remove_lines_containing($out_file2_svg_prefix.$re."_hist.svg", array("<dc:date>")); 
+    check_file($out_file2_svg_prefix.$re."_hist.svg", data_folder().$name."_out2_".$re."_hist.svg");
+}
+*/
 end_test();
 
 ?>
