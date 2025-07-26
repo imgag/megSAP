53726cb5c7d1f878e7390d89d1bbeab1b9de9c8d
KilianIlius <77616357+KilianIlius@users.noreply.github.com>
2025-07-26 16:11:41 +0200

    Ensure consistent fallback to data_folder when local_data copy is missing/incomplete (#415)
    * Added fallback if local vep cache and/or local samtools_ref_cache don't exist
    
    * Added no_sync flag for nested data_setup.php calls

--- src/Pipelines/somatic_tumor_normal.php
+++ src/Pipelines/somatic_tumor_normal.php
@@ -627,10 +627,7 @@ if (in_array("vi", $steps))
 			"-in_qcml ".$t_bam_map_qc,
 			"-threads ".$threads
 		];
-		if ($dedup_used)
-		{
-			$vc_viral_args[] = "-barcode_correction";
-		}
+		if ($dedup_used) $vc_viral_args[] = "-barcode_correction";
 		$parser->execTool("Tools/vc_viral_load.php", implode(" ", $vc_viral_args));
 	}
 }
