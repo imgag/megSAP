45163d547197a7ebe93207a35d3a02c7428ac258
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2025-07-27 07:53:59 +0200

    fixed output file

--- src/Tools/vc_deepvariant.php
+++ src/Tools/vc_deepvariant.php
@@ -104,12 +104,12 @@ $pipeline[] = ["", $parser->execApptainer("ngs-bits", "VcfLeftNormalize", "-stre
 
 //sort variants by genomic position
 $tmp_out = $parser->tempFile(".vcf");
-$pipeline[] = ["", $parser->execApptainer("ngs-bits", "VcfStreamSort", "-out $tmp_out", [], [], true)];
+$pipeline[] = ["", $parser->execApptainer("ngs-bits", "VcfStreamSort", "", [], [], true)];
 
 //fix error in VCF file and strip unneeded information
 if (contains($model_type, "PACBIO"))
 {
-	$pipeline[] = array("php ".repository_basedir()."/src/Tools/vcf_fix.php", "--deepvariant_mode", false);
+	$pipeline[] = array("php ".repository_basedir()."/src/Tools/vcf_fix.php", "--deepvariant_mode > $tmp_out", false);
 }
 
 //(2) execute pipeline
