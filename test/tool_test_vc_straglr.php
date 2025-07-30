d62407ba0783b6e730008ce5afef2c6a3da7da7c
Leon Schütz <leon.schuetz@med.uni-tuebingen.de>
2025-07-30 12:53:15 +0200

    settings*: updated straglrOn container
    test/tool_test_vc_straglr.php: added additional tests for allele plots
    
    data/tools/container_recipes/straglrOn_v0.2.4.def: updated container with old naming schema

--- test/tool_test_vc_straglr.php
+++ test/tool_test_vc_straglr.php
@@ -20,24 +20,12 @@ foreach(array("FXN", "ATXN3", "DMPK") as $re)
     check_file($out_file_svg_prefix.$re.".fa", data_folder().$name."_out1_".$re.".fa");
     remove_lines_containing($out_file_svg_prefix.$re."_hist.svg", array("<dc:date>")); 
     check_file($out_file_svg_prefix.$re."_hist.svg", data_folder().$name."_out1_".$re."_hist.svg");
+    remove_lines_containing($out_file_svg_prefix.$re.".svg", array("<dc:date>")); 
+    check_file($out_file_svg_prefix.$re.".svg", data_folder().$name."_out1_".$re.".svg");
 }
 /*
-$out_file_vcf = output_folder().$name."_out1.vcf";
-$out_file_bed = output_folder().$name."_out1.bed";
-$out_file_log = output_folder().$name."_out1.log";
-$out_file_svg_prefix = output_folder()."repeat_expansions/{$name}_out1_";
-$variant_catalog = data_folder().$name."_variant_catalog_in1.bed";
-check_exec("php ".src_folder()."/Tools/".$name.".php -in ".data_folder().$name."_in1.bam -out $out_file_bed -loci {$variant_catalog} --log {$out_file_log} -include_partials -test");
-remove_lines_containing($out_file_vcf, array("##fileDate=", "##reference="));
-check_file($out_file_vcf, data_folder().$name."_out1.vcf");
-check_file($out_file_bed, data_folder().$name."_out1.bed");
-foreach(array("FXN", "ATXN3", "DMPK") as $re)
-{
-    check_file($out_file_svg_prefix.$re.".fa", data_folder().$name."_out1_".$re.".fa");
-    remove_lines_containing($out_file_svg_prefix.$re."_hist.svg", array("<dc:date>")); 
-    check_file($out_file_svg_prefix.$re."_hist.svg", data_folder().$name."_out1_".$re."_hist.svg");
-}
 
+//TODO: create test for partial reads
 
 $out_file2_vcf = output_folder().$name."_out2.vcf";
 $out_file2_bed = output_folder().$name."_out2.bed";
