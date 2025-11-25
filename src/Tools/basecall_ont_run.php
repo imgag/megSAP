8bea161eb5aab6501267e78baa8db6a31eef9a1c
Leon Schütz <leon.schuetz@med.uni-tuebingen.de>
2025-11-25 10:30:32 +0100

    src/Tools/basecall_ont_run.php: added debug output for GPU check

--- src/Tools/basecall_ont_run.php
+++ src/Tools/basecall_ont_run.php
@@ -28,7 +28,11 @@ extract($parser->parse($argv));
 list($stdout, $stderr, $exit_code) = $parser->exec("nvidia-smi", "-L");
 if (($exit_code != 0) || (count($stdout) < 2) || (count($stderr) > 0))
 {
-	trigger_error("GPU check failed! Make sure nvidia-smi is installed and GPUs work properly!", E_USER_ERROR);
+	$parser->log("Exit Code:".$exit_code);
+	$parser->log("Stdout line:".count($stdout));
+	$parser->log("Stderr line:".count($stderr));
+	//TODO: change to error
+	trigger_error("GPU check failed! Make sure nvidia-smi is installed and GPUs work properly!", E_USER_WARNING);
 }
 
 //init
