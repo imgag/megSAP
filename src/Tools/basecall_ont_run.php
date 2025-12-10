0d22f3cfc5b3326894559dfcd7a6dd6231ed2755
Leon Schütz <leonschuetz@users.noreply.github.com>
2025-12-10 16:09:05 +0100

    ONT Basecalling (#467)
    * src/Tools/basecall_ont_run.php: new tool to run ONT basecalling on GPU servers
    
    * src/Tools/basecall_ont_run.php: allow definition of exact basecall model
    
    * src/Tools/basecall_ont_run.php:
        - fixed bug with basecalling without copy it to temp
        - added folder for basecall models
    
    * src/Tools/basecall_ont_run.php: added option for file-based calling
    
    settings.ini.default: added dorado and dorado model path
    
    * src/IMGAG/copy_ont_run.php: refactored script and added basecall support
    
    src/Tools/basecall_ont_run.php: added support for multiple run folders
    
    test/tool_test_copy_ont_run.php: updated tests
    
    * src/IMGAG/copy_ont_run.php: Bugfixes: create log file by default (beside run_folder), queue gpu jobs with to slots
    
    src/Tools/basecall_ont_run.php: Bugfixes: adjusted parameter type of secondary output and fixed copying
    
    * src/IMGAG/*_ont_run.php: set permissions for output folder/file
    
    * src/IMGAG/copy_ont_run.php: added info
    
    * src/IMGAG/copy_ont_run.php: added option to request a full re-calling
    
    src/Tools/basecall_ont_run.php: log server (env)
    
    * settings_nightly.ini: updated dorado version
    
    * src/IMGAG/copy_ont_run.php: typos
    
    src/Tools/runqc_parser_ont.php: Workaround for restarted runs
    
    * updated to latest master and fixed test
    
    * src/Tools/basecall_ont_run.php: added GPU server check
    
    src/IMGAG/copy_ont_run.php:
        - added option to ignore flowcell id
        - check basecall model for each subfolder separately
        - change output bam name
    
    * src/Tools/basecall_ont_run.php: added debug output for GPU check
    
    * updated test files
    
    * src/IMGAG/copy_ont_run.php: fail on partly basecalled runs
    
    src/Tools/runqc_parser_ont.php: added parameter to ignore flowcell id
    
    test/tool_test_copy_ont_run.php: added more tests
    
    * src/IMGAG/copy_ont_run.php: added email notification for basecall jobs
    
    * src/Tools/basecall_ont_run.php: fixed GPU server check
    
    * data/download_tools.sh: added dorado download
    
    * doc/development/update_overview.md: added dorado to the tool to update

---
+++ src/Tools/basecall_ont_run.php
@@ -0,0 +1,230 @@
+<?php 
+/** 
+	@page basecall_ont_run
+*/
+
+require_once(dirname($_SERVER['SCRIPT_FILENAME'])."/../Common/all.php");
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+$parser = new ToolBase("basecall_ont_run", "Performs dorado basecalling on ONT run folder.");
+$parser->addInfile("run_dir",  "Input data folder.", false);
+$parser->addOutfile("out_bam", "Output file path to store (basecalled) unmapped BAM.", false);
+
+//optional
+$parser->addString("basecall_model", "Model used for base calling: hac=high accuracy, sup=super accuracy", true, "hac");
+$parser->addInt("min_qscore", "Minimal read qScore for basecalling", true, 9);
+$parser->addFlag("queue_sample", "Queue analysis of the sample.");
+$parser->addFlag("skipped_only", "Use already basecalled BAMs and recall only skipped POD5s.");
+$parser->addFlag("copy_pod5", "Copy pod5 files to /tmp to reduce IO/network load.");
+$parser->addFlag("file_based_calling", "Call each POD5 file separately.");
+$parser->addString("secondary_output", "Folder to store a (additional) copy of the basecalled files (e.g. for backup)", true);
+$parser->addFlag("rename_skip_folder", "Rename the 'pod5_skip' folder(s) after basecalling.");
+
+extract($parser->parse($argv));
+
+
+//check GPU server
+list($stdout, $stderr, $exit_code) = $parser->exec("nvidia-smi", "-L");
+if (($exit_code != 0) || (count($stdout) < 2) || (count($stderr) > 1))
+{
+	$parser->log("Exit Code:".$exit_code);
+	$parser->log("Stdout line:".count($stdout));
+	$parser->log("Stderr line:".count($stderr));
+	$parser->log(implode("-", $stderr));
+	trigger_error("GPU check failed! Make sure nvidia-smi is installed and GPUs work properly!", E_USER_ERROR);
+}
+else
+{
+	trigger_error("GPU check passed!", E_USER_NOTICE);
+}
+
+//init
+
+//log server, user, etc.
+$parser->logServerEnvronment();
+
+//check basecall model
+if ($basecall_model == "hac")
+{
+	$basecall_model = "hac@".get_path("dorado_model_version").",5mCG_5hmCG";
+}
+else if ($basecall_model == "sup")
+{
+	$basecall_model = "sup@".get_path("dorado_model_version").",5mCG_5hmCG";
+}
+else
+{
+	trigger_error("Custom basecall model '{$basecall_model}' provided!", E_USER_WARNING);
+}
+//set ulimit
+exec2("ulimit -n 10000");
+
+//check secondary output
+if (isset($secondary_output) && ($secondary_output != ""))
+{
+	if (file_exists($secondary_output))
+	{
+		if (!is_dir($secondary_output)) trigger_error("Secondary output '{$secondary_output}' is not a directory!", E_USER_ERROR);
+	}
+	else
+	{
+		mkdir($secondary_output);
+	}
+}
+
+
+//find subdirectory in run directory
+$subdirs = array_values(array_diff(scandir($run_dir), array("..", ".")));
+if (count($subdirs) === 0)
+{
+	trigger_error("No subdirectories found in '{$run_dir}'.", E_USER_ERROR);
+}
+foreach ($subdirs as &$subdir)
+{
+	$subdir = "{$run_dir}/{$subdir}";
+}
+
+
+$bam_files = array();
+if ($skipped_only)
+{
+	//get BAMs
+	$bam_files = glob("{$run_dir}/*/bam_pass/*.bam");
+
+	if (count($bam_files) < 1) trigger_error("No BAM files found in run folder!", E_USER_ERROR);
+
+	//check model used for basecalling of bam files (test 10 random files)
+	for ($i=0; $i < 10; $i++) 
+	{ 
+		$idx = random_int(0, count($bam_files));
+		$model = get_basecall_model($bam_files[$idx]);
+
+		if (str_contains($basecall_model, "hac") && !str_contains($model, "hac")) trigger_error("Run should be basecalled with high accuracy, but already basecalled BAMs are not!" , E_USER_ERROR);
+		if (str_contains($basecall_model, "sup") && !str_contains($model, "sup")) trigger_error("Run should be basecalled with super accuracy, but already basecalled BAMs are not!" , E_USER_ERROR);
+	}
+
+}
+
+
+//get POD5 location
+$pod5_location_pattern = "{$run_dir}/*/pod5*/";
+if ($skipped_only) $pod5_location_pattern = "{$run_dir}/*/pod5_skip/";
+
+//copy POD5s
+$pod5_files = glob($pod5_location_pattern."*.pod5");
+if ($copy_pod5)
+{
+	$pod5_temp = $parser->tempFolder("pod5_temp");
+	foreach($pod5_files as $file)
+	{
+		$parser->copyFile($file, $pod5_temp."/".basename($file));
+	}
+
+	$pod5_location = [$pod5_temp];
+	$pod5_files = glob($pod5_temp."/*.pod5"); //use copied pod5s 
+}
+else
+{
+	$pod5_location = glob($pod5_location_pattern);
+}
+
+if (count($pod5_location) < 1) trigger_error("No POD5 files found in '{$pod5_location_pattern}'!", E_USER_ERROR);
+
+
+//TODO: create container
+// perform basecalling
+$bams_to_merge = [];
+$dorado_model_path = get_path("dorado_model_path");
+
+if ($file_based_calling)
+{
+	foreach ($pod5_files as $pod5_file) 
+	{
+		$tmp_bam = $parser->tempFile(basename2($pod5_file).".mod.unmapped.bam");
+		$parser->exec(get_path("dorado"), "basecaller --models-directory {$dorado_model_path} {$basecall_model} {$pod5_file} --min-qscore {$min_qscore} > {$tmp_bam}");
+		$bams_to_merge[] = $tmp_bam;
+	}
+}
+else
+{
+	foreach ($pod5_location as $folder) 
+	{
+		$tmp_bam = $parser->tempFile(".mod.unmapped.bam");
+		$parser->exec(get_path("dorado"), "basecaller --models-directory {$dorado_model_path} -r {$basecall_model} {$folder} --min-qscore {$min_qscore} > {$tmp_bam}");
+		$bams_to_merge[] = $tmp_bam;
+	}
+}
+
+
+if ($skipped_only)
+{
+	if (isset($secondary_output) && ($secondary_output != ""))
+	{
+		//copy BAMs to secondary output
+		foreach ($bams_to_merge as $bam_file) 
+		{
+			$parser->exec("cp", "{$bam_file} $secondary_output");
+		}
+
+	}
+	//add already basecalled bams
+	list($stdout, $stderr, $ec) = $parser->exec("find", "{$run_dir}/*/bam_pass/*.bam -name '*.bam' -type f");
+
+	$bams_to_merge = array_merge($bams_to_merge, $stdout);
+}
+
+
+if (count($bams_to_merge) > 1)
+{
+	//merge bams and copy to output location
+	$merged_bam = $parser->tempFile(".merged.mod.unmapped.bam");
+	$bam_list = $parser->tempFile(".bams_to_merge.txt");
+	file_put_contents($bam_list, implode("\n", $bams_to_merge));
+	$dorado_model_path = get_path("dorado_model_path");
+	$parser->execApptainer("samtools", "samtools cat", "--threads 20 -o {$merged_bam} -b {$bam_list}", [$run_dir], []);
+
+	
+	//copy output
+	$parser->copyFile($merged_bam, $out_bam);
+	if (isset($secondary_output) && ($secondary_output != "") && !$skipped_only)
+	{
+		$parser->exec("cp", "{$merged_bam} $secondary_output");
+	}
+
+}
+else
+{
+	//only copy necessary
+	$parser->copyFile($bams_to_merge[0], $out_bam);
+	if (isset($secondary_output) && ($secondary_output != "") && !$skipped_only)
+	{
+		$parser->exec("cp", $bams_to_merge[0]." $secondary_output");
+	}
+}
+
+//move pod5_skip folder
+if ($skipped_only && $rename_skip_folder)
+{
+	$skip_folder = glob("{$run_dir}/*/pod5_skip", GLOB_ONLYDIR);
+	foreach ($skip_folder as $folder) 
+	{
+		$parser->moveFile($folder, $folder."_done");
+	}
+}
+
+//set file permissions for output BAM
+$parser->exec("chmod", "775 {$out_bam}");
+$parser->exec("chgrp", "f_ad_bi_l_medgen_access_storages {$out_bam}", true, false);
+
+
+//queue sample
+if ($queue_sample)
+{
+	$sample_name = basename2($out_bam);
+	$parser->execTool("Tools/db_queue_analysis.php", "-samples {$sample_name} -type 'single sample'");
+}
+
+
+
+?>
--- src/Tools/runqc_parser_ont.php
+++ src/Tools/runqc_parser_ont.php
@@ -12,6 +12,7 @@ $parser->addString("name", "Name of the run.", false);
 $parser->addString("run_dir", "Run directory.", false);
 $parser->addFlag("force", "Overwrites already existing DB entries instead of throwing an error.");
 $parser->addEnum("db",  "Database to connect to.", true, db_names(), "NGSD");
+$parser->addFlag("ignore_flowcell_id_check", "Ignores Errors due to mismatching flowcell IDs between NGSD and folder name (e.g. if flowcell was replaced during sequencing)");
 $parser->addFlag("no_db", "No NGSD import, only report run QC to STDOUT.");
 extract($parser->parse($argv));
 
@@ -29,131 +30,227 @@ $flowcell_id = $result[0]['fcid'];
 if (!ends_with($run_dir, substr($name, 1))) trigger_error("Run folder name ('".basename2($run_dir)."') doesn't match run name ({$name}!", E_USER_ERROR);
 
 // check fcid and raw data folder
-$fc_dir = glob("{$run_dir}/2*_{$flowcell_id}_*");
-if (count($fc_dir) == 0) trigger_error("Run raw data folder missing or wrong FlowCell ID in NGSD!", E_USER_ERROR);
-if (count($fc_dir) > 1) 
+if ($ignore_flowcell_id_check) $fc_dirs = glob("{$run_dir}/2*_*_*");
+else $fc_dirs = glob("{$run_dir}/2*_{$flowcell_id}_*");
+if (count($fc_dirs) == 0) trigger_error("Run raw data folder missing or wrong FlowCell ID in NGSD!", E_USER_ERROR);
+if (count($fc_dirs) > 1) trigger_error("Multiple raw data folders found!\n".implode(", ", $fc_dirs), E_USER_WARNING);
+
+$run_qc_values = array();
+
+foreach ($fc_dirs as $fc_dir) 
 {
-	trigger_error("Multiple raw data found!\n".implode(", ", $fc_dir)."\n Choosing biggest folder...", E_USER_WARNING);
-	$folders = array();
-	foreach ($fc_dir as $folder)
-	{ 
-		list($stdout, $stderr, $exit_code) = $parser->exec("du", "-b --summarize {$folder}");
-		$folder_size = intval(explode("\t", $stdout[0])[0]);
-		$folders[$folder] = $folder_size;
-	}
-	$fc_dir = array_keys($folders, max($folders))[0];
-	trigger_error("Using folder '{$fc_dir}' for QC import.", E_USER_NOTICE);
-}
-else
-{
-	//only one folder present 
-	$fc_dir = $fc_dir[0];
-}
+	
+	// check report file
+	if ($ignore_flowcell_id_check) $report_files = glob("{$fc_dir}/report_*_*.json");
+	else $report_files = glob("{$fc_dir}/report_{$flowcell_id}_*.json");
+	if (count($report_files) == 0) trigger_error("No report JSON file found!", E_USER_ERROR);
+	if (count($report_files) > 1) trigger_error("Multiple report JSON files found!\n".implode(", ", $report_files) , E_USER_ERROR);
 
+	//parse report file
+	$report_content = (array) json_decode(file_get_contents($report_files[0], true));
 
-// check report file
-$report_files = glob("{$fc_dir}/report_{$flowcell_id}_*.json");
-if (count($report_files) == 0) trigger_error("No report JSON file found!", E_USER_ERROR);
-if (count($report_files) > 1) trigger_error("Multiple report JSON files found!\n".implode(", ", $report_files) , E_USER_ERROR);
-
-//parse report file
-$report_content = (array) json_decode(file_get_contents($report_files[0], true));
-
-//extract run info
-$run_info = $report_content["protocol_run_info"];
-$protocol_id = $run_info->protocol_id;
-$software_args = (array) $run_info->args;
-$device_firmware_versions = array();
-foreach ((array) $run_info->device->firmware_version as $component) 
-{
-	$device_firmware_versions[] = $component->component.":".$component->version;
-}
-$minknow_version = $run_info->software_versions->minknow->full;
-
-
-//use the last acquisition to extract runQC
-$final_acquisition = end($report_content["acquisitions"]);
-
-//check for correct acquisition
-$stop_reason = $final_acquisition->acquisition_run_info->stop_reason;
-if ($stop_reason != "STOPPED_PROTOCOL_ENDED") trigger_error("Invalid stop reason '{$stop_reason}'!", E_USER_WARNING);
-
-
-$read_num = $final_acquisition->acquisition_run_info->yield_summary->read_count;
-$yield = $final_acquisition->acquisition_run_info->yield_summary->basecalled_pass_bases + $final_acquisition->acquisition_run_info->yield_summary->basecalled_fail_bases;
-$basecalled_pass_bases = $final_acquisition->acquisition_run_info->yield_summary->basecalled_pass_bases;
-$passing_filter_perc = ($basecalled_pass_bases / $yield) * 100.0;
-
-// check for non-basecalled bases
-$fraction_skipped = 0.0;
-if (isset($final_acquisition->acquisition_run_info->yield_summary->fraction_skipped)) 
-{
-	$fraction_skipped = $final_acquisition->acquisition_run_info->yield_summary->fraction_skipped;
-}
-if ($fraction_skipped > 1e-5) trigger_error(($fraction_skipped*100)."% of bases are not basecalled! Consider manual basecalling!", E_USER_WARNING);
-
-
-// get q30/q20 values
-$qscore_histograms_bases = ((array) $final_acquisition->qscore_histograms)[1];
-if ($qscore_histograms_bases->bucket_value_type != "QScore_BasecalledBases") trigger_error("QScore histogram not found!", E_USER_ERROR);
-
-//get qscores of buckets
-$qscores = array();
-$ranges = (array) $qscore_histograms_bases->bucket_ranges;
-foreach($ranges as $range)
-{
-	if (isset($range->end)) $qscores[] = $range->end;
-	else $qscores[] = PHP_INT_MAX; //last bucket has no end	 
-}
-
-//get combined counts of buckets
-$combined_counts = array_fill(0, count($qscores), 0);
-$histogram_data = (array) $qscore_histograms_bases->histogram_data;
-foreach($histogram_data as $data_object)
-{
-	$counts = (array) $data_object->bucket_values;
-	if (count($qscores) != count($counts)) trigger_error("Count size doesn't match number of buckets! (".count($counts)."vs".count($qscores).")", E_USER_ERROR);
-	for ($i=0; $i < count($qscores); $i++) 
-	{ 
-		$combined_counts[$i] += $counts[$i];
-	}
-}
-//calculate q30 value
-$all_bases = 0;
-$q30_bases = 0;
-$q20_bases = 0;
-for ($i=0; $i < count($qscores); $i++) 
-{ 
-	$all_bases += $combined_counts[$i];
-	if ($qscores[$i] >= 30) $q30_bases += $combined_counts[$i];
-	if ($qscores[$i] >= 20) $q20_bases += $combined_counts[$i];
-}
-$q30_perc = ($q30_bases/$all_bases) * 100.0;
-$q20_perc = ($q20_bases/$all_bases) * 100.0;
-
-//get N50 value
-$n50 = -1;
-$read_length_histograms = (array) $final_acquisition->read_length_histogram;
-foreach ($read_length_histograms as $histogram)
-{
-	if (isset($histogram->read_length_type) && ($histogram->read_length_type == "BasecalledBases") 
-		&& isset($histogram->bucket_value_type) && ($histogram->bucket_value_type == "ReadLengths"))
+	//extract run info
+	$run_info = $report_content["protocol_run_info"];
+	$protocol_id = $run_info->protocol_id;
+	$software_args = (array) $run_info->args;
+	$device_firmware_versions = array();
+	foreach ((array) $run_info->device->firmware_version as $component) 
 	{
-		// var_dump($histogram->plot->histogram_data);
-		$n50 = $histogram->plot->histogram_data[0]->n50;
-		break;
+		$device_firmware_versions[] = $component->component.":".$component->version;
 	}
+	$minknow_version = $run_info->software_versions->minknow->full;
+
+
+	//use the last acquisition to extract runQC
+	$final_acquisition = end($report_content["acquisitions"]);
+
+	//check for correct acquisition
+	$stop_reason = $final_acquisition->acquisition_run_info->stop_reason;
+	if ($stop_reason != "STOPPED_PROTOCOL_ENDED") trigger_error("Invalid stop reason '{$stop_reason}'!", E_USER_WARNING);
+
+
+	$read_num = $final_acquisition->acquisition_run_info->yield_summary->read_count;
+	$yield = $final_acquisition->acquisition_run_info->yield_summary->basecalled_pass_bases + $final_acquisition->acquisition_run_info->yield_summary->basecalled_fail_bases;
+	$basecalled_pass_bases = $final_acquisition->acquisition_run_info->yield_summary->basecalled_pass_bases;
+	$passing_filter_perc = ($basecalled_pass_bases / $yield) * 100.0;
+
+	// check for non-basecalled bases
+	$fraction_skipped = 0.0;
+	if (isset($final_acquisition->acquisition_run_info->yield_summary->fraction_skipped)) 
+	{
+		$fraction_skipped = $final_acquisition->acquisition_run_info->yield_summary->fraction_skipped;
+	}
+	if ($fraction_skipped > 1e-5) trigger_error(($fraction_skipped*100)."% of bases are not basecalled! Consider manual basecalling!", E_USER_WARNING);
+
+
+	// get q30/q20 values
+	$qscore_histograms_bases = ((array) $final_acquisition->qscore_histograms)[1];
+	if ($qscore_histograms_bases->bucket_value_type != "QScore_BasecalledBases") trigger_error("QScore histogram not found!", E_USER_ERROR);
+
+	//get qscores of buckets
+	$qscores = array();
+	$ranges = (array) $qscore_histograms_bases->bucket_ranges;
+	foreach($ranges as $range)
+	{
+		if (isset($range->end)) $qscores[] = $range->end;
+		else $qscores[] = PHP_INT_MAX; //last bucket has no end	 
+	}
+
+	//get combined counts of buckets
+	$combined_counts = array_fill(0, count($qscores), 0);
+	$histogram_data = (array) $qscore_histograms_bases->histogram_data;
+	foreach($histogram_data as $data_object)
+	{
+		$counts = (array) $data_object->bucket_values;
+		if (count($qscores) != count($counts)) trigger_error("Count size doesn't match number of buckets! (".count($counts)."vs".count($qscores).")", E_USER_ERROR);
+		for ($fc_dir_name=0; $fc_dir_name < count($qscores); $fc_dir_name++) 
+		{ 
+			$combined_counts[$fc_dir_name] += $counts[$fc_dir_name];
+		}
+	}
+	//calculate q30 value
+	$all_bases = 0;
+	$q30_bases = 0;
+	$q20_bases = 0;
+	for ($fc_dir_name=0; $fc_dir_name < count($qscores); $fc_dir_name++) 
+	{ 
+		$all_bases += $combined_counts[$fc_dir_name];
+		if ($qscores[$fc_dir_name] >= 30) $q30_bases += $combined_counts[$fc_dir_name];
+		if ($qscores[$fc_dir_name] >= 20) $q20_bases += $combined_counts[$fc_dir_name];
+	}
+	$q30_perc = ($q30_bases/$all_bases) * 100.0;
+	$q20_perc = ($q20_bases/$all_bases) * 100.0;
+
+	//get N50 value
+	$n50 = -1;
+	$read_length_histograms = (array) $final_acquisition->read_length_histogram;
+	foreach ($read_length_histograms as $histogram)
+	{
+		if (isset($histogram->read_length_type) && ($histogram->read_length_type == "BasecalledBases") 
+			&& isset($histogram->bucket_value_type) && ($histogram->bucket_value_type == "ReadLengths"))
+		{
+			// var_dump($histogram->plot->histogram_data);
+			$n50 = $histogram->plot->histogram_data[0]->n50;
+			break;
+		}
+	}
+
+	if ($n50 < 0) trigger_error("N50 value not found!", E_USER_ERROR);
+
+	// collapse arrays to single string
+	$software_args = implode(" ", $software_args);
+	$device_firmware_versions = implode(";",$device_firmware_versions);
+
+
+	//store per-folder QCs:
+	$folder_qc = array();
+	$folder_qc["run"] = $name;
+	$folder_qc["fc_id"] = $flowcell_id;
+	$folder_qc["read_num"] = $read_num;
+	$folder_qc["yield"] = $yield;
+	$folder_qc["passing_filter_perc"] = $passing_filter_perc; 
+	$folder_qc["fraction_skipped"] = $fraction_skipped;
+	$folder_qc["q30_perc"] = $q30_perc;
+	$folder_qc["q20_perc"] = $q20_perc;
+	$folder_qc["N50"] = $n50;
+	$folder_qc["protocol_id"] = $protocol_id; 
+	$folder_qc["software_args"] = $software_args;
+	$folder_qc["device_firmware_versions"] = $device_firmware_versions;
+	$folder_qc["minknow_version"] = $minknow_version;
+
+	$run_qc_values[basename($fc_dir)] = $folder_qc;
 }
 
-if ($n50 < 0) trigger_error("N50 value not found!", E_USER_ERROR);
+//get total/combined values
+$flowcell_ids = array();
+$total_read_num = 0;
+$total_yield = 0;
+$passing_filter_percentages = array();
+$fraction_skipped_list = array();
+$q30_percentages = array();
+$q20_percentages = array();
+$n50s = array();
+$protocol_ids = array();
+$software_arguments = array();
+$device_firmware_version_list = array();
+$minknow_versions = array();
 
-// collapse arrays to single string
-$software_args = implode(" ", $software_args);
-$device_firmware_versions = implode(";",$device_firmware_versions);
+foreach ($run_qc_values as $fc_dir_name => $folder_qc) 
+{
+	$flowcell_ids[] = $folder_qc["fc_id"];
+	$total_read_num += $folder_qc["read_num"];
+	$total_yield += $folder_qc["yield"];
+	$passing_filter_percentages[] = $folder_qc["passing_filter_perc"];
+	$fraction_skipped_list[] = $folder_qc["fraction_skipped"];
+	$q30_percentages[] = $folder_qc["q30_perc"];
+	$q20_percentages[] = $folder_qc["q20_perc"];
+	$n50s[] = $folder_qc["N50"];
+	$protocol_ids[] = $folder_qc["protocol_id"];
+	$software_arguments[] = $folder_qc["software_args"];
+	$device_firmware_version_list[] = $folder_qc["device_firmware_versions"];
+	$minknow_versions[] = $folder_qc["minknow_version"];
+}
+
+//check argument fields
+if (count(array_unique($flowcell_ids)) > 1) trigger_error("Flocell IDs of sub folders differ!\n".implode(", ", $flowcell_ids), E_USER_ERROR);
+$flowcell_id = array_unique($flowcell_ids)[0];
+if (count(array_unique($protocol_ids)) > 1) trigger_error("Protocol IDs of sub folders differ!\n".implode(", ", $protocol_ids), E_USER_ERROR);
+$protocol_id = array_unique($protocol_ids)[0];
+//workaround for restarted runs with FAST
+if (count(array_unique($software_arguments)) > 1) trigger_error("Software arguments of sub folders differ!\n".implode(", ", $software_arguments), E_USER_WARNING); 
+$software_args = array_unique($software_arguments)[0];
+if (count(array_unique($device_firmware_version_list)) > 1) trigger_error("Device firmware versions of sub folders differ!\n".implode(", ", $device_firmware_version_list), E_USER_ERROR);
+$device_firmware_versions = array_unique($device_firmware_version_list)[0];
+if (count(array_unique($minknow_versions)) > 1) trigger_error("MinKNOW versions of sub folders differ!\n".implode(", ", $minknow_versions), E_USER_ERROR);
+$minknow_version = array_unique($minknow_versions)[0];
+
+//calculate weight factors for yield and read numbers
+$wf_reads = array();
+$wf_yield = array();
+foreach ($run_qc_values as $fc_dir_name => $folder_qc) 
+{
+	$wf_reads[$fc_dir_name] = floatval($folder_qc["read_num"] / $total_read_num);
+	$wf_yield[$fc_dir_name] = floatval($folder_qc["yield"] / $total_yield);
+}
+
+//calculate weighted QC values
+$read_num = $total_read_num;
+$yield = $total_yield;
+$passing_filter_perc = 0.00;
+$fraction_skipped = 0.00;
+$q30_perc = 0.00;
+$q20_perc = 0.00;
+$n50 = 0;
+foreach ($run_qc_values as $fc_dir_name => $folder_qc)
+{ 
+	$passing_filter_perc += $wf_yield[$fc_dir_name] * $run_qc_values[$fc_dir_name]["passing_filter_perc"];
+	$fraction_skipped += $wf_yield[$fc_dir_name] * $run_qc_values[$fc_dir_name]["fraction_skipped"];
+	$q30_perc += $wf_yield[$fc_dir_name] * $run_qc_values[$fc_dir_name]["q30_perc"];
+	$q20_perc += $wf_yield[$fc_dir_name] * $run_qc_values[$fc_dir_name]["q20_perc"];
+	$n50 += $wf_reads[$fc_dir_name] * $run_qc_values[$fc_dir_name]["N50"];
+}
 
 //print TSV to STDOUT
-print "#run\tfc_id\tread_num\tyield\tpassing_filter_perc\tfraction_skipped\tq30_perc\tq20_perc\tN50\tprotocol_id\tsoftware_args\tdevice_firmware_versions\tminknow_version\n";
-print "{$name}\t{$flowcell_id}\t{$read_num}\t{$yield}\t{$passing_filter_perc}\t{$fraction_skipped}\t{$q30_perc}\t{$q20_perc}\t{$n50}\t{$protocol_id}\t{$software_args}\t{$device_firmware_versions}\t{$minknow_version}\n";
+print "#run\tfc_id\tread_num\tyield\tpassing_filter_perc\tfraction_skipped\tq30_perc\tq20_perc\tN50\tprotocol_id\tsoftware_args\tdevice_firmware_versions\tminknow_version\tsub_folder\n";
+foreach ($run_qc_values as $fc_dir_name => $folder_qc) 
+{
+	$parts = array($name);
+	$parts[] = $folder_qc["fc_id"];
+	$parts[] = $folder_qc["read_num"];
+	$parts[] = $folder_qc["yield"];
+	$parts[] = $folder_qc["passing_filter_perc"];
+	$parts[] = $folder_qc["fraction_skipped"];
+	$parts[] = $folder_qc["q30_perc"];
+	$parts[] = $folder_qc["q20_perc"];
+	$parts[] = $folder_qc["N50"];
+	$parts[] = $folder_qc["protocol_id"];
+	$parts[] = $folder_qc["software_args"];
+	$parts[] = $folder_qc["device_firmware_versions"];
+	$parts[] = $folder_qc["minknow_version"];
+	$parts[] = $fc_dir_name;
+	print substr(implode("\t", $parts), 1)."\n";
+}
+print substr($name, 1)."\t{$flowcell_id}\t{$read_num}\t{$yield}\t{$passing_filter_perc}\t{$fraction_skipped}\t{$q30_perc}\t{$q20_perc}\t{$n50}\t{$protocol_id}\t{$software_args}\t{$device_firmware_versions}\t{$minknow_version}\tcombined\n";
 
 
 if (!$no_db)
