78ef6019dd1d9aaa6003a64f75dcf82c10c12383
Leon Schütz <leon.schuetz@med.uni-tuebingen.de>
2025-12-11 11:18:30 +0100

    data/download_dbs.sh: added download of EpicV2 ids
    src/Pipelines/analyze_longread.php: added creation of methylation file for Epigen
    
    test/data_longread/*: added Epigen test

--- src/Pipelines/analyze_longread.php
+++ src/Pipelines/analyze_longread.php
@@ -97,6 +97,7 @@ $lowcov_file = $folder."/".$name."_".$sys["name_short"]."_lowcov.bed";
 //methylation 
 $modkit_track = $folder."/".$name."_modkit_track.bed.gz";
 $modkit_summary = $folder."/".$name."_modkit_summary.txt";
+$epigen_tsv = $folder."/".$name."_epigen.tsv";
 //variant calling
 $vcf_file = $folder."/".$name."_var.vcf.gz";
 $vcf_file_annotated = $folder."/".$name."_var_annotated.vcf.gz";
@@ -715,6 +716,7 @@ if (in_array("me", $steps))
 	if (!contains_methylation($used_bam_or_cram)) trigger_error("BAM file doesn't contain methylation info! Skipping step 'me'", E_USER_WARNING);
 	else 
 	{
+		// create methylartist plots
 		$args = [];
 		$args[] = "-folder {$folder}";
 		$args[] = "-name {$name}";
@@ -734,6 +736,12 @@ if (in_array("me", $steps))
 			$args[] = "-regions {$methyl_regions}";
 		}
 		$parser->execTool("Tools/create_methyl_plot.php", implode(" ", $args));
+
+
+		// create Epigen TSV file
+		$epic_id_file = get_path("data_folder")."/dbs/illumina-epicids/EPIC-8v2-0_A1.csv";
+		$parser->execApptainer("ngs-bits", "BedToEpigen", "-in {$modkit_track} -out {$epigen_tsv} -id_file {$epic_id_file} -sample {$name}", [$epic_id_file, $modkit_track], [$epigen_tsv]);
+
 	}
 }
 
