cfed8ad3bce54cf7ca914a3810749928b8ac4c80
Leon Schütz <leon.schuetz@med.uni-tuebingen.de>
2026-02-02 15:05:09 +0100

    test/*: Bugfix: fixed vc_clair tool test and longread pipeline test in nightly test environment

--- test/tool_test_vc_clair.php
+++ test/tool_test_vc_clair.php
@@ -21,7 +21,9 @@ check_file(output_folder()."LSK109_01_var.vcf.gz", data_folder().$name."_out2.vc
 ########################## SQK-LSK114 mito-calling ##################
 $model1 = get_path("clair3_models")."/r1041_e82_400bps_hac_g632/";
 check_exec("php ".src_folder()."/Tools/{$name}.php -bam ".data_folder().$name."_in3.bam -folder ".output_folder()." -target ".data_folder().$name."_in_roi.bed -name LSK114_02 -model {$model1} -threads 4 --log ".output_folder().$name."_out3.log");
-check_file(output_folder()."LSK114_02_var.vcf.gz", data_folder().$name."_out3.vcf.gz");
+# remove unreliably varian chrMT:7337 G>A
+exec("zcat ".output_folder()."LSK114_02_var.vcf.gz | egrep -v '^chrMT\t7337\t' | bgzip -c > ".output_folder()."LSK114_02_var_modified.vcf.gz");
+check_file(output_folder()."LSK114_02_var_modified.vcf.gz", data_folder().$name."_out3.vcf.gz");
 
 end_test();
 
