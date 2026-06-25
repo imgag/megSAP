60eb6472e352de1737b293291e96e9f36d48ebea
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2026-06-25 10:15:55 +0200

    udpated ngs-bits

--- data/download_dbs.sh
+++ data/download_dbs.sh
@@ -179,7 +179,7 @@ mkdir -p REVEL
 cd REVEL
 wget -O revel-v1.3_all_chromosomes.zip https://zenodo.org/record/7072866/files/revel-v1.3_all_chromosomes.zip
 unzip -p revel-v1.3_all_chromosomes.zip | php $src/Install/db_converter_revel.php > tmp.vcf
-singularity exec $ngsbits VcfSort -in tmp.vcf -out REVEL_1.3.vcf
+singularity exec $ngsbits VcfSort -in tmp.vcf -out REVEL_1.3.vcf -split_chrs
 rm tmp.vcf
 singularity exec $htslib bgzip REVEL_1.3.vcf
 singularity exec $htslib tabix -f -C -m 9 -p vcf REVEL_1.3.vcf.gz
@@ -191,13 +191,13 @@ mkdir -p AlphaMissense
 cd AlphaMissense
 wget -O AlphaMissense_hg38.tsv.gz https://storage.googleapis.com/dm_alphamissense/AlphaMissense_hg38.tsv.gz
 php $src/Install/db_converter_alphamissense.php AlphaMissense_hg38.tsv.gz > AlphaMissense_hg38.vcf
-singularity exec $ngsbits VcfSort -in AlphaMissense_hg38.vcf -out AlphaMissense_hg38.vcf
+singularity exec $ngsbits VcfSort -in AlphaMissense_hg38.vcf -out AlphaMissense_hg38.vcf -split_chrs
 singularity exec $htslib bgzip AlphaMissense_hg38.vcf
 singularity exec $htslib tabix -p vcf AlphaMissense_hg38.vcf.gz
 #also download isoforms file as backup - for some genes the main file does not contain anything, e.g. PRPH2
 wget -O AlphaMissense_isoforms_hg38.tsv.gz https://storage.googleapis.com/dm_alphamissense/AlphaMissense_isoforms_hg38.tsv.gz
 php $src/Install/db_converter_alphamissense.php AlphaMissense_isoforms_hg38.tsv.gz > AlphaMissense_isoforms_hg38.vcf
-singularity exec $ngsbits VcfSort -in AlphaMissense_isoforms_hg38.vcf -out AlphaMissense_isoforms_hg38.vcf
+singularity exec $ngsbits VcfSort -in AlphaMissense_isoforms_hg38.vcf -out AlphaMissense_isoforms_hg38.vcf -split_chrs
 singularity exec $htslib bgzip AlphaMissense_isoforms_hg38.vcf
 singularity exec $htslib tabix -p vcf AlphaMissense_isoforms_hg38.vcf.gz
 
