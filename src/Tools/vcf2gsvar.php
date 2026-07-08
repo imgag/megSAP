eed297e4b93a8bc0755211f1cee1e6770b8ac6e7
Jakob Admard <jakob.admard@med.uni-tuebingen.de>
2026-07-08 12:12:39 +0200

    vcf2gsvar.php: fix handling of DRAGEN calls with multiple alleles

--- src/Tools/vcf2gsvar.php
+++ src/Tools/vcf2gsvar.php
@@ -730,24 +730,20 @@ while(!gzeof($handle))
 		//special handling of DRAGEN targeted calls (can have multiple alleles)
 		if (isset($info["TARGETED"]))
 		{
-			$affected_alleles = array_sum(explode("|", strtr($sample["GT"], array("."=>"0", "/"=>"|"))));
-			if ($affected_alleles == 0)
+			$alleles = explode("|", strtr($sample["GT"], array("."=>"0", "/"=>"|")));
+			$alt_alleles = array_sum($alleles);
+			if ($alt_alleles == 0)
 			{
 				$genotype = "wt";
-			} 
-			else if ($affected_alleles == 1)
+			}
+			else if ($alt_alleles < count($alleles))
 			{
 				$genotype = "het";
 			}
-			else if ($affected_alleles == 2)
+			else
 			{
 				$genotype = "hom";
 			}
-			else 
-			{
-				trigger_error("VCF contains invalid genotype '".$sample["GT"]."'!", E_USER_ERROR);	
-			}
-
 		}
 		else
 		{
